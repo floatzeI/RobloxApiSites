0ccf784acdaeac8c2fe1db67fc7620041ddebe3c
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-11 17:30:43 -0400

    add AccountStatus enum

---
+++ Platform/Membership/Roblox.Platform.Membership/AccountStatus.cs
@@ -0,0 +1,30 @@
+﻿namespace Roblox.Platform.Membership
+{
+    public enum AccountStatus
+    {
+        /// <summary>
+        /// Account is normal
+        /// </summary>
+        Ok = 1,
+        /// <summary>
+        /// Temporary ban, hidden from search
+        /// </summary>
+        Suppressed,
+        /// <summary>
+        /// Permanently banned (aka terminated)
+        /// </summary>
+        Deleted,
+        /// <summary>
+        /// Same as <see cref="AccountStatus.Deleted"/> as far as I can tell; not used anymore aside from accounts already with this status
+        /// </summary>
+        Poisoned,
+        /// <summary>
+        /// Locked account
+        /// </summary>
+        MustValidateEmail,
+        /// <summary>
+        /// GPDR Deleted (essentially terminated but user must be 100% hidden from all endpoints - as if they don't exist!)
+        /// </summary>
+        Forgotten,
+    }
+}
