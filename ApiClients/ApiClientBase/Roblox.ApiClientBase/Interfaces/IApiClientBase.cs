ef8c80725b2a344c758bb391cc43b20dbe79e5a3
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-11 17:28:14 -0400

    add api client base interface (loosely based on stack traces; unfinished)

---
+++ ApiClients/ApiClientBase/Roblox.ApiClientBase/Interfaces/IApiClientBase.cs
@@ -0,0 +1,19 @@
+﻿// // Copyright 2021 RblxTrade - All Rights Reserved
+
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Roblox.ApiClientBase
+{
+    public interface IApiClientBase
+    {
+        void ThrowConvertedException(ApiResponseException ex, string actionName, int? statusCode, string statusDescription,
+            string actionPath);
+
+        Task<ApiClientResponse> ExecuteHttpRequest(string actionPath, HttpMethod method, Dictionary<string, string> queryStringParameters,
+            Dictionary<string, string> formParameters, Dictionary<string, string> headers, string rawPostData, Dictionary<string, object> multipartFormData,
+            string actionName);
+    }
+}
