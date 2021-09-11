21c629deb4149592608016f7858c66767e28b204
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-11 17:36:46 -0400

    add AccountInformationEntry model

---
+++ Services/Roblox.Services/Models/AccountInformation.cs
@@ -0,0 +1,9 @@
+namespace Roblox.Services.Models.AccountInformation
+{
+    public class AccountInformationEntry
+    {
+        public long userId { get; set; }
+        public string description { get; set; }
+
+    }
+}
