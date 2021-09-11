befa68552b567cae162ae52240cb98e1bcf0b3ec
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-11 17:28:31 -0400

    add api response class

---
+++ ApiClients/ApiClientBase/Roblox.ApiClientBase/ApiClientResponse.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http.Headers;
+
+namespace Roblox.ApiClientBase
+{
+    public class ApiClientResponse
+    {
+        public string machineId { get; set; }
+        public string url { get; set; }
+        public string body { get; set; }
+        public HttpResponseHeaders headers { get; set; }
+        public HttpStatusCode statusCode { get; set; }
+    }
+}
