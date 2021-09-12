20a5a9191cbe503b244144a930c63973231e2a7a
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-12 15:27:10 -0400

    add CreateSession();

--- Services/Roblox.Services/Controllers/SessionsController.cs
+++ Services/Roblox.Services/Controllers/SessionsController.cs
@@ -3,20 +3,26 @@ using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Roblox.Services.Models;
+using Roblox.Services.Models.Sessions;
 using Roblox.Services.Services;
 
 namespace Roblox.Services.Controllers
 {
     [ApiController]
-    [Route("/Users/v1")]
+    [Route("/Sessions/v1")]
     [ProducesErrorResponseType(typeof(ErrorResponse))]
     public class SessionsController
     {
-        private IUsersService usersService { get; }
-        public SessionsController(Services.IUsersService service)
+        private ISessionsService sessionService { get; }
+        public SessionsController(Services.ISessionsService service)
         {
-            usersService = service;
+            sessionService = service;
         }
-        
+
+        [HttpPost("CreateSessionForAccount")]
+        public async Task<CreateSessionResponse> CreateSession([Required] long userId)
+        {
+            return await sessionService.CreateSession(userId);
+        }
     }
 }
