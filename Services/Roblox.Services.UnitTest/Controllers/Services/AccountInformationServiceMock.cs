31cd4729bdc356b52410069844cc0c356c860e16
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-11 23:02:11 -0400

    add mock for testing

---
+++ Services/Roblox.Services.UnitTest/Controllers/Services/AccountInformationServiceMock.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Roblox.Services.Models.AccountInformation;
+using Roblox.Services.Services;
+
+namespace Roblox.Services.UnitTest.Controllers.Services
+{
+    public class AccountInformationServiceMock : IAccountInformationService
+    {
+        public AccountDescriptionEntry getDescriptionMockData { get; set; }
+        public async Task<AccountDescriptionEntry> GetDescription(long userId)
+        {
+            return getDescriptionMockData;
+        }
+    }
+}
