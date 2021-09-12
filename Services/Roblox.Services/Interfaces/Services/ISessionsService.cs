20986c88580a9ad0183b5eb044540cd56cd7b56c
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-12 15:59:08 -0400

    add DeleteSession(); add docs;

--- Services/Roblox.Services/Interfaces/Services/ISessionsService.cs
+++ Services/Roblox.Services/Interfaces/Services/ISessionsService.cs
@@ -5,6 +5,17 @@ namespace Roblox.Services.Services
 {
     public interface ISessionsService
     {
+        /// <summary>
+        /// Create session for the userId.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
         Task<CreateSessionResponse> CreateSession(long userId);
+
+        /// <summary>
+        /// Delete a session by its ID
+        /// </summary>
+        /// <param name="sessionIdWithPrefix">The sessionId, with a prefix</param>
+        Task DeleteSession(string sessionIdWithPrefix);
     }
 }
