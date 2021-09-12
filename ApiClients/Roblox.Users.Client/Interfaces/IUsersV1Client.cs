9e5bdfcff28cbe962e804e3935bc82db08ca26f1
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-12 14:00:56 -0400

    add SetDescription();

--- ApiClients/Roblox.Users.Client/Interfaces/IUsersV1Client.cs
+++ ApiClients/Roblox.Users.Client/Interfaces/IUsersV1Client.cs
@@ -11,5 +11,12 @@ namespace Roblox.Users.Client
         /// <param name="agentId">The ID of the user to fetch the description record for</param>
         /// <returns>The user's <see cref="Roblox.Users.Client.Models.Responses.DescriptionResponse" /></returns>
         Task<DescriptionResponse> GetDescription(long agentId);
+        
+        /// <summary>
+        /// Set the description for the user
+        /// </summary>
+        /// <param name="agentId">The user to set the description for</param>
+        /// <param name="newDescription">The user's new description</param>
+        Task SetDescription(long agentId, string newDescription);
     }
 }
