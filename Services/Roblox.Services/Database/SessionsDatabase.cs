db45e7473a7ef98fd13a1391bd146bf23bb20231
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-12 15:26:42 -0400

    implement InsertSession();

--- Services/Roblox.Services/Database/SessionsDatabase.cs
+++ Services/Roblox.Services/Database/SessionsDatabase.cs
@@ -1,7 +1,29 @@
+using System;
+using System.Threading.Tasks;
+using Dapper;
+using Roblox.Services.Models.Sessions;
+
 namespace Roblox.Services.Database
 {
     public class SessionsDatabase : ISessionsDatabase
     {
-        
+        public async Task<SessionEntry> InsertSession(long userId, string sessionId)
+        {
+            var currentTime = DateTime.Now;
+            await Db.client.ExecuteAsync("INSERT INTO account_session (user_id, id, created_at, updated_at) VALUES (@user_id, @id, @created_at, @updated_at)", new
+            {
+                user_id = userId,
+                id = sessionId,
+                created_at = currentTime,
+                updated_at = currentTime,
+            });
+            return new()
+            {
+                userId = userId,
+                id = sessionId,
+                updated = currentTime,
+                created = currentTime,
+            };
+        }
     }
 }
