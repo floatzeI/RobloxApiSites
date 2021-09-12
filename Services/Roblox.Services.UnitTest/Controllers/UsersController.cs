1b6eed1481ab3c9e9ae6b184d08d466d04b3b05e
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-12 11:34:50 -0400

    [refactor] switch to moq

--- Services/Roblox.Services.UnitTest/Controllers/UsersController.cs
+++ Services/Roblox.Services.UnitTest/Controllers/UsersController.cs
@@ -1,28 +1,30 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Roblox.Services.Controllers;
-using Roblox.Services.UnitTest.Controllers.Services;
 using Xunit;
+using Moq;
+using Roblox.Services.Models.Users;
+using Roblox.Services.Services;
 
 namespace Roblox.Services.UnitTest.Controllers
 {
     public class UnitTestUsersController
     {
-        private static UsersServiceMock service = new();
-        private static UsersController controller = new(service);
         
         [Fact]
         public async Task Return_Mock_User_Description()
         {
             var userId = 1;
             var expectedDescription = "This is an example description for a unit test.";
-            service.getDescriptionMockData = new()
+            var mock = new Mock<IUsersService>();
+            mock.Setup(foo => foo.GetDescription(userId)).ReturnsAsync(new UserDescriptionEntry()
             {
                 userId = userId,
+                description = expectedDescription,
                 created = DateTime.Now,
                 updated = DateTime.Now,
-                description = expectedDescription
-            };
+            });
+            UsersController controller = new(mock.Object);
             var result = await controller.GetUserDescription(1);
             Assert.Equal(expectedDescription, result.description);
             Assert.Equal(userId, result.userId);
