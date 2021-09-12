cdbce4ca9de373063e87ce5209b65c688522dec3
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-12 15:26:34 -0400

    add InsertSession();

--- Services/Roblox.Services/Interfaces/Database/ISessionsDatabase.cs
+++ Services/Roblox.Services/Interfaces/Database/ISessionsDatabase.cs
@@ -1,7 +1,10 @@
+using System.Threading.Tasks;
+using Roblox.Services.Models.Sessions;
+
 namespace Roblox.Services.Database
 {
     public interface ISessionsDatabase
     {
-        
+        Task<SessionEntry> InsertSession(long userId, string sessionId);
     }
 }
