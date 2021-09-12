89329b6a7c91fad4d6b9a82bb589a7d866333b7c
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-11 23:02:17 -0400

    add AccountDescriptionEntry

--- Services/Roblox.Services/Models/AccountInformation.cs
+++ Services/Roblox.Services/Models/AccountInformation.cs
@@ -1,9 +1,18 @@
+using System;
+
 namespace Roblox.Services.Models.AccountInformation
 {
     public class AccountInformationEntry
     {
         public long userId { get; set; }
         public string description { get; set; }
+    }
 
+    public class AccountDescriptionEntry
+    {
+        public long userId { get; set; }
+        public string description { get; set; }
+        public DateTime created { get; set; }
+        public DateTime updated { get; set; }
     }
 }
