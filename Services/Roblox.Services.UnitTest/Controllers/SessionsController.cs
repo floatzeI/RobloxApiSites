2633743c69e6a3104830f62dfcbd02ca4fc46f12
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-12 15:58:54 -0400

    add Delete_Session_For_User();

--- Services/Roblox.Services.UnitTest/Controllers/SessionsController.cs
+++ Services/Roblox.Services.UnitTest/Controllers/SessionsController.cs
@@ -1,10 +1,11 @@
 using System.Threading.Tasks;
 using Moq;
+using Roblox.Services.Controllers;
 using Roblox.Services.Models.Sessions;
 using Roblox.Services.Services;
 using Xunit;
 
-namespace Roblox.Services.Controllers
+namespace Roblox.Services.UnitTest.Controllers
 {
     public class UnitTestSessionsController
     {
@@ -23,6 +24,18 @@ namespace Roblox.Services.Controllers
             var result = await sessionsController.CreateSession(userId);
             Assert.Equal(userId, result.userId);
             Assert.Equal(expectedSessionId, result.sessionId);
+            mock.Verify();
+        }
+
+        [Fact]
+        public async Task Delete_Session_For_User()
+        {
+            var sessionId = SessionsService.sessionCookiePrefix + "SessionIdHere";
+            var mock = new Mock<ISessionsService>();
+            mock.Setup(c => c.DeleteSession(sessionId));
+            var controller = new SessionsController(mock.Object);
+            await controller.DeleteSession(sessionId);
+            mock.VerifyAll();
         }
     }
 }
