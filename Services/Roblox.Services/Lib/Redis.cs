597ef0a5104af1a7a32fbae91d8ad170dc7e272e
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-12 13:08:44 -0400

    add Redlock.net

--- Services/Roblox.Services/Lib/Redis.cs
+++ Services/Roblox.Services/Lib/Redis.cs
@@ -1,10 +1,13 @@
 using System;
+using System.Collections.Generic;
 using Npgsql;
 using Dapper;
 using SqlKata;
 using SqlKata.Execution;
 using SqlKata.Compilers;
 using System.Data.SqlClient;
+using RedLockNet.SERedis;
+using RedLockNet.SERedis.Configuration;
 using StackExchange.Redis;
 
 namespace Roblox.Services
@@ -13,6 +16,7 @@ namespace Roblox.Services
     {
         private static string connectionString { get; set; }
         public static ConnectionMultiplexer client { get; set; }
+        public static RedLockFactory redlockFactory { get; set; }
 
         public static void SetConnectionString(string newConnectionString)
         {
@@ -23,6 +27,12 @@ namespace Roblox.Services
 
             connectionString = newConnectionString;
             client = ConnectionMultiplexer.Connect(newConnectionString);
+            // todo: we will likely want to support multiple ConnectionMultiplexers in the future for high availability... see: https://redis.io/topics/distlock
+            var multiplexers = new List<RedLockMultiplexer>
+            {
+                client,
+            };
+            redlockFactory = RedLockFactory.Create(multiplexers);
         }
     }
 }
