f087d3694f6a309dd21299f5b125419450badc35
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-12 12:53:14 -0400

    add SetDescriptionRequest; add AccountInformationEntry;

--- Services/Roblox.Services/Models/Users.cs
+++ Services/Roblox.Services/Models/Users.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Roblox.Services.Models.Users
 {
@@ -6,6 +7,26 @@ namespace Roblox.Services.Models.Users
     {
         public long userId { get; set; }
         public string description { get; set; }
+        public DateTime created { get; set; }
+        public DateTime updated { get; set; }
+    }
+
+    public class SetDescriptionRequest
+    {
+        [Required]
+        public long userId { get; set; }
+        [Required]
+        public string description { get; set; }
+    }
+
+    public class AccountInformationEntry
+    {
+        public long userId { get; set; }
+        public string description { get; set; }
+        public int? gender { get; set; }
+        public int? birthYear { get; set; }
+        public int? birthMonth { get; set; }
+        public int? birthDay { get; set; }
         public DateTime created { get; set; }
         public DateTime updated { get; set; }
     }
