4c8c7d7c15a546040e076dfafa37da1abfdfc987
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-11 23:03:50 -0400

    add init function (currently unused)

---
+++ Services/Roblox.Services.UnitTest/Main.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Roblox.Services.UnitTest
+{
+    public class MainUnitTestInitialize
+    {
+        public MainUnitTestInitialize()
+        {
+            // TODO: init here, if we ever need to...
+        }
+    }
+}
