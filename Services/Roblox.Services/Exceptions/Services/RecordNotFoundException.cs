91bf512b6f62930a234de7b6e89424763a04f338
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-11 23:04:24 -0400

    add RecordNotFoundException;

---
+++ Services/Roblox.Services/Exceptions/Services/RecordNotFoundException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Roblox.Services.Exceptions.Services
+{
+    public class RecordNotFoundException : Exception
+    {
+        public RecordNotFoundException(long recordPrimaryKey) : base("Could not find record with ID = " +
+                                                                     recordPrimaryKey)
+        {
+            
+        }
+        
+        public RecordNotFoundException() : base("Could not find record")
+        {
+            
+        }
+    }
+}
