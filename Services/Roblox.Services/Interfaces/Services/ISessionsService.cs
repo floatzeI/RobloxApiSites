af859dc228a43e49649e06d2f55c702467897ec2
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-12 15:26:55 -0400

    add CreateSession();

--- Services/Roblox.Services/Interfaces/Services/ISessionsService.cs
+++ Services/Roblox.Services/Interfaces/Services/ISessionsService.cs
@@ -1,7 +1,10 @@
+using System.Threading.Tasks;
+using Roblox.Services.Models.Sessions;
+
 namespace Roblox.Services.Services
 {
     public interface ISessionsService
     {
-        
+        Task<CreateSessionResponse> CreateSession(long userId);
     }
 }
