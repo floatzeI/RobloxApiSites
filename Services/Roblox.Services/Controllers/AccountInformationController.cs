178dca9c897ff3d23c604b52acfa0f2d6d6fe43e
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-11 23:04:17 -0400

    add GetUserDescription();

---
+++ Services/Roblox.Services/Controllers/AccountInformationController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Roblox.Services.Services;
+
+namespace Roblox.Services.Controllers
+{
+    [ApiController]
+    [Route("/User/v1")]
+    public class AccountInformationController
+    {
+        private IAccountInformationService accountInformationService { get; }
+        public AccountInformationController(Services.IAccountInformationService service)
+        {
+            accountInformationService = service;
+        }
+        
+        [HttpGet("GetUserDescription")]
+        public async Task<Models.AccountInformation.AccountDescriptionEntry> GetUserDescription(long userId)
+        {
+            return  await accountInformationService.GetDescription(userId);
+        }
+    }
+}
