6e8dd9493b7035b4f19a3d7bbd8e05631f5a6df0
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-11 23:04:30 -0400

    add AccountInformationService interface

---
+++ Services/Roblox.Services/Interfaces/Services/IAccountInformationService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Roblox.Services.Services
+{
+    public interface IAccountInformationService
+    {
+        Task<Models.AccountInformation.AccountDescriptionEntry> GetDescription(long userId);
+    }
+}
