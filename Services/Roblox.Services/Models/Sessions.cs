97303e23b7c65861ca954befec316f3388978fce
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-12 15:27:04 -0400

    add SessionEntry; add CreateSessionResponse;

--- Services/Roblox.Services/Models/Sessions.cs
+++ Services/Roblox.Services/Models/Sessions.cs
@@ -1,4 +1,18 @@
+using System;
+
 namespace Roblox.Services.Models.Sessions
 {
-    
+    public class SessionEntry
+    {
+        public string id { get; set; }
+        public long userId { get; set; }
+        public DateTime created { get; set; }
+        public DateTime updated { get; set; }
+    }
+
+    public class CreateSessionResponse
+    {
+        public long userId { get; set; }
+        public string sessionId { get; set; }
+    }
 }
