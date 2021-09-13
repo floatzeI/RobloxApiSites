1bc6a83182a52e8f6c62decdb19fd876c0cb37e8
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-13 09:30:12 -0400

    add SetUserBirthDate(); add GetDateTimeFromBirthDate(); add CreateUser(); add DeleteUser();

--- Services/Roblox.Services/Interfaces/Services/IUsersService.cs
+++ Services/Roblox.Services/Interfaces/Services/IUsersService.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 
 namespace Roblox.Services.Services
 {
@@ -6,6 +7,10 @@ namespace Roblox.Services.Services
     {
         Task<Models.Users.UserDescriptionEntry> GetDescription(long userId);
         Task SetUserDescription(long userId, string description);
+        Task SetUserBirthDate(long userId, DateTime birthDate);
         Task<Models.Users.UserInformationResponse> GetUserById(long userId);
+        DateTime GetDateTimeFromBirthDate(int birthYear, int birthMonth, int birthDay);
+        Task<Models.Users.UserAccountEntry> CreateUser(string username);
+        Task DeleteUser(long userId);
     }
 }
