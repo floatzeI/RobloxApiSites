bb18b69bc101f37d675bcbe36d9bde2ebe812280
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-13 15:18:53 -0400

    add AuthenticationController (notimplemented)

---
+++ ApiSites/Roblox.Authentication.Api/Roblox.Authentication.Api/Controllers/V1/AuthenticationController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Roblox.Authentication.Api.Controllers
+{
+    [ApiController]
+    [Route("/v1/")]
+    public class AuthenticationController : ControllerBase
+    {
+        [HttpPost("login")]
+        public async Task Login()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
