9f6c6c4062bc50b04fcd70055c1e198eebd82cba
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-12 20:17:03 -0400

    add GetSession(); add ReportSessionUsage();

--- Services/Roblox.Services/Interfaces/Services/ISessionsService.cs
+++ Services/Roblox.Services/Interfaces/Services/ISessionsService.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 using Roblox.Services.Models.Sessions;
+using Roblox.Services.Exceptions.Services;
 
 namespace Roblox.Services.Services
 {
@@ -17,5 +18,19 @@ namespace Roblox.Services.Services
         /// </summary>
         /// <param name="sessionIdWithPrefix">The sessionId, with a prefix</param>
         Task DeleteSession(string sessionIdWithPrefix);
+
+        /// <summary>
+        /// Get a session by the session id
+        /// </summary>
+        /// <param name="sessionIdWithPrefix">The id of the session, including the cookie prefix</param>
+        /// <returns>A session object</returns>
+        /// <exception cref="RecordNotFoundException">SessionID does not exist</exception>
+        Task<SessionEntry> GetSession(string sessionIdWithPrefix);
+
+        /// <summary>
+        /// Update the "updated_at" value for the session id to the current time
+        /// </summary>
+        /// <param name="sessionIdWithPrefix">The ID of the session to update, with prefix</param>
+        Task ReportSessionUsage(string sessionIdWithPrefix);
     }
 }
