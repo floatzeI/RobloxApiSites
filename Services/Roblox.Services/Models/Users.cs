574351c6401b68b8ce9c6ad6047d5e9bc284146c
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-12 20:58:57 -0400

    add UserInformationResponse; add UserAccountEntry;

--- Services/Roblox.Services/Models/Users.cs
+++ Services/Roblox.Services/Models/Users.cs
@@ -1,5 +1,6 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using Roblox.Platform.Membership;
 
 namespace Roblox.Services.Models.Users
 {
@@ -27,6 +28,27 @@ namespace Roblox.Services.Models.Users
         public int? birthYear { get; set; }
         public int? birthMonth { get; set; }
         public int? birthDay { get; set; }
+        public DateTime created { get; set; }
+        public DateTime updated { get; set; }
+    }
+
+    public class UserInformationResponse
+    {
+        public long userId { get; set; }
+        public string username { get; set; }
+        public string displayName { get; set; }
+        public string description { get; set; }
+        public AccountStatus accountStatus { get; set; }
+        public DateTime birthdate { get; set; }
+        public DateTime created { get; set; }
+    }
+
+    public class UserAccountEntry
+    {
+        public long userId { get; set; }
+        public string username { get; set; }
+        public string displayName { get; set; }
+        public AccountStatus accountStatus { get; set; }
         public DateTime created { get; set; }
         public DateTime updated { get; set; }
     }
