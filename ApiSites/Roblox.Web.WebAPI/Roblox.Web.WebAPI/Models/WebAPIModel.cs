ab28a9536c79c6997295a23feeecdee11eb3d839
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-13 18:28:01 -0400

    add error response model;

--- ApiSites/Roblox.Web.WebAPI/Roblox.Web.WebAPI/Models/WebAPIModel.cs
+++ ApiSites/Roblox.Web.WebAPI/Roblox.Web.WebAPI/Models/WebAPIModel.cs
@@ -1,4 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+
 namespace Roblox.Web.WebAPI
 {
     public abstract class ApiEmptyResponseModel { }
+
+    public class ErrorResponse
+    {
+        public IEnumerable<ErrorEntry> errors { get; set; }
+    }
+
+    public class ErrorEntry
+    {
+        public int code { get; set; }
+        public string message { get; set; }
+        public string userFacingMessage { get; set; }
+    }
 }
