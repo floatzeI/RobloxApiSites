10558024604cf460d62530c9544e2046c016943f
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-12 20:24:13 -0400

    rename tests for consistency; add get and report usage tests;

--- Services/Roblox.Services.IntegrationTest/Controllers/SessionsController.cs
+++ Services/Roblox.Services.IntegrationTest/Controllers/SessionsController.cs
@@ -1,6 +1,10 @@
+using System;
+using System.Globalization;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 using Roblox.Services.Controllers;
 using Roblox.Services.Database;
+using Roblox.Services.Exceptions.Services;
 using Roblox.Services.Services;
 using Xunit;
 
@@ -8,26 +12,53 @@ namespace Roblox.Services.IntegrationTest.Controllers
 {
     public class IntegrationTestSessionsController : IntegrationTestBase
     {
+        private SessionsController controller { get; set; } = new SessionsController(new SessionsService(new SessionsDatabase(new DatabaseConfiguration<dynamic>(new PostgresDatabaseProvider(), null))));
         [Fact]
-        public async Task CreateSession()
+        public async Task Create_Session()
         {
             var sessionUserId = 1;
-            var controller = new SessionsController(new SessionsService(new SessionsDatabase(new DatabaseConfiguration<dynamic>(new PostgresDatabaseProvider(), null))));
             var result = await controller.CreateSession(sessionUserId);
             Assert.Equal(sessionUserId, result.userId);
             Assert.True(result.sessionId.Length > 500);
         }
+        
         [Fact]
-        public async Task DeleteSession()
+        public async Task Delete_Session_By_Id()
         {
             // Create a random session
             var sessionUserId = 1;
-            var controller = new SessionsController(new SessionsService(new SessionsDatabase(new (new PostgresDatabaseProvider(), null))));
             var result = await controller.CreateSession(sessionUserId);
             // Delete it
             await controller.DeleteSession(result.sessionId);
             // Assert it is deleted
-            // todo: get session by id and confirm does not exist
+            await Assert.ThrowsAsync<RecordNotFoundException>(async () =>
+            {
+                await controller.GetSession(result.sessionId);
+            });
+        }
+        
+        [Fact]
+        public async Task Get_Session_By_Id()
+        {
+            var sessionUserId = 1;
+            var mySession = await controller.CreateSession(sessionUserId);
+            // get the session and confirm everything matches
+            var data = await controller.GetSession(mySession.sessionId);
+            Assert.Equal(sessionUserId, data.userId);
+            Assert.Equal(mySession.sessionId, data.id);
+        }
+
+        [Fact]
+        public async Task Report_Session_Usage()
+        {
+            var sessionUserId = 1;
+            var mySession = await controller.CreateSession(sessionUserId);
+            // increment updated at
+            await Task.Delay(TimeSpan.FromSeconds(1));
+            await controller.ReportSessionUsage(mySession.sessionId);
+            // get the id and confirm it updated
+            var data = await controller.GetSession(mySession.sessionId);
+            Assert.NotEqual(data.created.ToString(CultureInfo.InvariantCulture), data.updated.ToString(CultureInfo.InvariantCulture));
         }
     }
 }
