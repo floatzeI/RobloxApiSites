0cb850c5286adafcf5a29951cf6589c54ff46fde
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-13 12:32:06 -0400

    add statuscode

--- ApiClients/ApiClientBase/Roblox.ApiClientBase/Exceptions/ApiClientException.cs
+++ ApiClients/ApiClientBase/Roblox.ApiClientBase/Exceptions/ApiClientException.cs
@@ -1,12 +1,14 @@
 using System;
+using System.Net;
 
 namespace Roblox.ApiClientBase
 {
     public class ApiClientException : System.Exception
     {
+        public HttpStatusCode statusCode { get; set; }
         public ApiClientException(string url, int statusCode, string statusDescription, string machineId, string responseText, Exception innerException = null) : base("ApiClient Exception:\nURL = " + url + "\nStatusCode = " + statusCode + "\nStatusDescription = " + statusDescription + "\nResponse Machine Id = ?\nResponseText = " + responseText, innerException)
         {
-            
+            this.statusCode = (HttpStatusCode)statusCode;
         }
     }
 }
