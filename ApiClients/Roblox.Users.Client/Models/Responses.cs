a57c157e22a7b0ea232286cc7a0465a8eb08b4b7
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-13 12:15:11 -0400

    add GetUserByIdEntry;

--- ApiClients/Roblox.Users.Client/Models/Responses.cs
+++ ApiClients/Roblox.Users.Client/Models/Responses.cs
@@ -1,4 +1,5 @@
 using System;
+using Roblox.Platform.Membership;
 
 namespace Roblox.Users.Client.Models.Responses
 {
@@ -8,5 +9,15 @@ namespace Roblox.Users.Client.Models.Responses
         public string description { get; set; }
         public DateTime created { get; set; }
         public DateTime updated { get; set; }
+    }
+
+    public class GetUserByIdEntry
+    {
+        public long userId { get; set; }
+        public string username { get; set; }
+        public string displayName { get; set; }
+        public AccountStatus accountStatus { get; set; }
+        public DateTime birthDate { get; set; }
+        public DateTime created { get; set; }
     }
 }
