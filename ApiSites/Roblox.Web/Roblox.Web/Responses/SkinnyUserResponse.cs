253f9b836bea857a8e448fcd83e95fd5807a2ddf
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-13 16:18:53 -0400

    add SkinnyUserResponse

---
+++ ApiSites/Roblox.Web/Roblox.Web/Responses/SkinnyUserResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Roblox.Web.Responses
+{
+    public class SkinnyUserResponse
+    {
+        public long id { get; set; }
+        public string name { get; set; }
+        public string displayName { get; set; }
+    }
+}
