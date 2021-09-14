89c8569d55dd12d5d27c64d71b3ae1feb16697b6
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-14 14:35:39 -0400

    add SignupResponseCodes

---
+++ ApiSites/Roblox.Web.Authentication/Roblox.Web.Authentication/Enums/Signup.cs
@@ -0,0 +1,10 @@
+namespace Roblox.Web.Authentication.Signup
+{
+    public enum SignupResponseCodes
+    {
+        InvalidBirthDay = 4,
+        UsernameTaken = 6,
+        PasswordTooSimple = 9,
+        InvalidPassword = 7,
+    }
+}
