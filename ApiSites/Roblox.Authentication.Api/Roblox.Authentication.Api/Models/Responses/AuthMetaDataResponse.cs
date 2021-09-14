6cf9c3941919fe75354e6f985a2c310bf180234d
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-13 22:02:33 -0400

    add AuthMetaDataResponse

---
+++ ApiSites/Roblox.Authentication.Api/Roblox.Authentication.Api/Models/Responses/AuthMetaDataResponse.cs
@@ -0,0 +1,10 @@
+namespace Roblox.Authentication.Api.Models
+{
+    public class AuthMetaDataResponse
+    {
+        /// <summary>
+        /// Timeout for cookie law notice banner
+        /// </summary>
+        public int cookieLawNoticeTimeout { get; set; } = 20000;
+    }
+}
