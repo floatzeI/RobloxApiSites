bc645e9a44780aea286f02e76f35b61a5947a14a
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-13 21:31:53 -0400

    add CreateSessionResponse

--- ApiClients/Roblox.Sessions.Client/Models/Responses.cs
+++ ApiClients/Roblox.Sessions.Client/Models/Responses.cs
@@ -9,4 +9,10 @@ namespace Roblox.Sessions.Client.Models.Responses
         public DateTime created { get; set; }
         public DateTime updated { get; set; }
     }
+
+    public class CreateSessionResponse
+    {
+        public long userId { get; set; }
+        public string sessionId { get; set; }
+    }
 }
