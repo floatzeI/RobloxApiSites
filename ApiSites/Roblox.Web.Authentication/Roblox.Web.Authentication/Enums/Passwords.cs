f31caddb69db6d198dd17875b7e5d726a53570a5
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-14 10:30:58 -0400

    add Roblox.Web.Authentication project

---
+++ ApiSites/Roblox.Web.Authentication/Roblox.Web.Authentication/Enums/Passwords.cs
@@ -0,0 +1,10 @@
+namespace Roblox.Web.Authentication.Passwords
+{
+    public enum PasswordResponseCodes
+    {
+        InvalidCurrentPassword = 1,
+        InvalidPassword,
+        PinLocked,
+        Flooded,
+    }
+}
---
+++ ApiSites/Roblox.Web.Authentication/Roblox.Web.Authentication/Enums/Recovery.cs
@@ -0,0 +1,7 @@
+namespace Roblox.Web.Authentication.Recovery
+{
+    public enum RecoveryResponseCodes
+    {
+        RobloxWeChatApiUnavailable = 7,
+    }
+}
