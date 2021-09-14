8ad775b8e03ca6ce8041f40d57daca82f0507ac4
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-13 22:04:04 -0400

    add DeleteSession

--- ApiClients/Roblox.Sessions.Client/Interfaces/ISessionsV1Client.cs
+++ ApiClients/Roblox.Sessions.Client/Interfaces/ISessionsV1Client.cs
@@ -5,11 +5,18 @@ namespace Roblox.Sessions.Client
     public interface ISessionsV1Client
     {
         Task<Models.Responses.GetSessionByIdResponse> GetSessionById(string sessionId);
+        
         /// <summary>
         /// Create a session for the account. Returns the session ID (aka session cookie).
         /// </summary>
         /// <param name="userId">The user to create a session for</param>
         /// <returns>The sessionId</returns>
         Task<string> CreateSession(long userId);
+        
+        /// <summary>
+        /// Delete a session by its ID (aka cookie value)
+        /// </summary>
+        /// <param name="sessionId">The session ID (aka cookie)</param>
+        Task DeleteSession(string sessionId);
     }
 }
