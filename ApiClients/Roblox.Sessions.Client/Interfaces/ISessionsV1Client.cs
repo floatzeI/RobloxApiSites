1eac9939ea1ab064f88e864a146fea766a810c29
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-13 21:29:33 -0400

    add CreateSession();

--- ApiClients/Roblox.Sessions.Client/Interfaces/ISessionsV1Client.cs
+++ ApiClients/Roblox.Sessions.Client/Interfaces/ISessionsV1Client.cs
@@ -5,5 +5,11 @@ namespace Roblox.Sessions.Client
     public interface ISessionsV1Client
     {
         Task<Models.Responses.GetSessionByIdResponse> GetSessionById(string sessionId);
+        /// <summary>
+        /// Create a session for the account. Returns the session ID (aka session cookie).
+        /// </summary>
+        /// <param name="userId">The user to create a session for</param>
+        /// <returns>The sessionId</returns>
+        Task<string> CreateSession(long userId);
     }
 }
