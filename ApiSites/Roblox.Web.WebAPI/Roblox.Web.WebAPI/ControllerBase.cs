c617ae701bffceef4e44d52289e5564c3c8ccb52
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-14 12:10:53 -0400

    add _userOverrideForTests

--- ApiSites/Roblox.Web.WebAPI/Roblox.Web.WebAPI/ControllerBase.cs
+++ ApiSites/Roblox.Web.WebAPI/Roblox.Web.WebAPI/ControllerBase.cs
@@ -4,6 +4,11 @@ namespace Roblox.Web.WebAPI.Controllers
 {
     public class ApiControllerBase : Microsoft.AspNetCore.Mvc.ControllerBase
     {
-        public IUser authenticatedUser => (IUser)HttpContext.Items["AuthenticatedUser"];
+        public IUser _userOverrideForTests { get; set; }
+        
+        public IUser authenticatedUser
+        {
+            get => _userOverrideForTests ?? (IUser)HttpContext.Items["AuthenticatedUser"];
+        }
     }
 }
