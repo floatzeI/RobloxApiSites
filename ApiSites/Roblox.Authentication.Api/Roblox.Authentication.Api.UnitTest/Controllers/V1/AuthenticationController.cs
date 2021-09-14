22137a7cf571d3fd3b943d23bdada52152b29893
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-14 12:19:48 -0400

    add more login test cases;

--- ApiSites/Roblox.Authentication.Api/Roblox.Authentication.Api.UnitTest/Controllers/V1/AuthenticationController.cs
+++ ApiSites/Roblox.Authentication.Api/Roblox.Authentication.Api.UnitTest/Controllers/V1/AuthenticationController.cs
@@ -3,10 +3,12 @@ using Moq;
 using Roblox.Authentication.Api.Controllers;
 using Roblox.Authentication.Api.Exceptions;
 using Roblox.Passwords.Client;
+using Roblox.Passwords.Client.Exceptions;
 using Roblox.Platform.Authentication;
 using Roblox.Platform.Membership;
 using Roblox.Sessions.Client;
 using Roblox.Users.Client;
+using Roblox.Users.Client.Exceptions;
 using Roblox.Users.Client.Models.Responses;
 using Xunit;
 
@@ -152,6 +154,147 @@ namespace Roblox.Authentication.Api.UnitTest.Controllers.V1
             passwordsClient.Verify(c => c.IsPasswordCorrect(userId, password), Times.Once);
             usersClient.Verify(c => c.GetUserById(userId), Times.Once);
             sessionsClient.Verify(c => c.CreateSession(userId), Times.Never);
+        }
+        
+         
+        [Fact]
+        public async Task Login_With_Invalid_Username_And_Fail()
+        {
+            // arguments
+            var username = "Roblox";
+            var password = "Roblox123";
+            
+            // mocks
+            var usersClient = new Mock<IUsersV1Client>();
+            var passwordsClient = new Mock<IPasswordsV1Client>();
+            var sessionsClient = new Mock<ISessionsV1Client>();
+
+            usersClient.Setup(c => c.GetUserByUsername(username)).ThrowsAsync(new UserNotFoundException());
+
+            // create and run
+            var controller = new AuthenticationController(usersClient.Object, passwordsClient.Object, sessionsClient.Object);
+            // Assert
+            await Assert.ThrowsAsync<IncorrectCredentialsException>(async () =>
+            {
+                await controller.Login(new()
+                {
+                    ctype = CredentialsType.Username,
+                    cvalue = username,
+                    password = password,
+                });
+            });
+        }
+        
+        [Fact]
+        public async Task Login_With_Incorrect_Password_And_Fail()
+        {
+            // arguments
+            var username = "Roblox";
+            var password = "Roblox123";
+            var userId = 1;
+            
+            // mocks
+            var usersClient = new Mock<IUsersV1Client>();
+            var passwordsClient = new Mock<IPasswordsV1Client>();
+            var sessionsClient = new Mock<ISessionsV1Client>();
+
+            usersClient.Setup(c => c.GetUserByUsername(username)).ReturnsAsync(new SkinnyUserEntry()
+            {
+                username = username,
+                userId = userId,
+            });
+            passwordsClient.Setup(c => c.IsPasswordCorrect(userId, password)).ReturnsAsync(false);
+            usersClient.Setup(c => c.GetUserById(userId)).ReturnsAsync(new GetUserByIdEntry()
+            {
+                username = username,
+                userId = userId,
+                accountStatus = AccountStatus.Ok,
+            });
+            
+            // create and run
+            var controller = new AuthenticationController(usersClient.Object, passwordsClient.Object, sessionsClient.Object);
+            // Assert
+            await Assert.ThrowsAsync<IncorrectCredentialsException>(async () =>
+            {
+                await controller.Login(new()
+                {
+                    ctype = CredentialsType.Username,
+                    cvalue = username,
+                    password = password,
+                });
+            });
+            // verify
+            passwordsClient.Verify(c => c.IsPasswordCorrect(userId, password), Times.Once);
+            sessionsClient.Verify(c => c.CreateSession(userId), Times.Never);
+        }
+        
+        [Fact]
+        public async Task Login_To_Account_With_No_Password_And_Fail()
+        {
+            // arguments
+            var username = "Roblox";
+            var password = "Roblox123";
+            var userId = 1;
+            
+            // mocks
+            var usersClient = new Mock<IUsersV1Client>();
+            var passwordsClient = new Mock<IPasswordsV1Client>();
+            var sessionsClient = new Mock<ISessionsV1Client>();
+
+            usersClient.Setup(c => c.GetUserByUsername(username)).ReturnsAsync(new SkinnyUserEntry()
+            {
+                username = username,
+                userId = userId,
+            });
+            passwordsClient.Setup(c => c.IsPasswordCorrect(userId, password))
+                .ThrowsAsync(new UserHasNoPasswordException());
+            usersClient.Setup(c => c.GetUserById(userId)).ReturnsAsync(new GetUserByIdEntry()
+            {
+                username = username,
+                userId = userId,
+                accountStatus = AccountStatus.Forgotten,
+            });
+            
+            // create and run
+            var controller = new AuthenticationController(usersClient.Object, passwordsClient.Object, sessionsClient.Object);
+            // Assert
+            await Assert.ThrowsAsync<LoginAccountIssueException>(async () =>
+            {
+                await controller.Login(new()
+                {
+                    ctype = CredentialsType.Username,
+                    cvalue = username,
+                    password = password,
+                });
+            });
+            // verify
+            passwordsClient.Verify(c => c.IsPasswordCorrect(userId, password), Times.Once);
+            sessionsClient.Verify(c => c.CreateSession(userId), Times.Never);
+        }
+                
+        [Fact]
+        public async Task Login_With_Unsupported_Method_And_Fail_Temporary()
+        {
+            // arguments
+            var password = "Roblox123";
+            
+            // mocks
+            var usersClient = new Mock<IUsersV1Client>();
+            var passwordsClient = new Mock<IPasswordsV1Client>();
+            var sessionsClient = new Mock<ISessionsV1Client>();
+
+            // create and run
+            var controller = new AuthenticationController(usersClient.Object, passwordsClient.Object, sessionsClient.Object);
+            // Assert
+            await Assert.ThrowsAsync<CredentialsNotSuitableForLogin>(async () =>
+            {
+                await controller.Login(new()
+                {
+                    ctype = CredentialsType.PhoneNumber,
+                    cvalue = "18005555555",
+                    password = password,
+                });
+            });
         }
     }
 }
