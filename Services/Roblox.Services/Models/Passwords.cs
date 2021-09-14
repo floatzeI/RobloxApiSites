e29355d0c0ed81e167b417f032e6b84927405803
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-13 21:00:18 -0400

    add ValidatePasswordRequest; add ValidatePasswordResponse;

--- Services/Roblox.Services/Models/Passwords.cs
+++ Services/Roblox.Services/Models/Passwords.cs
@@ -9,4 +9,15 @@ namespace Roblox.Services.Models.Passwords
         public DateTime created { get; set; }
         public DateTime updated { get; set; }
     }
+
+    public class ValidatePasswordRequest
+    {
+        public long userId { get; set; }
+        public string password { get; set; }
+    }
+    
+    public class ValidatePasswordResponse
+    {
+        public bool isCorrect { get; set; }
+    }
 }
