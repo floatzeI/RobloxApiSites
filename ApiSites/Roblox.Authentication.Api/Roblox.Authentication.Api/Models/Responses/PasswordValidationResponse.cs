9350a992436f2b26a88ce2331e5efff2675a4f75
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-14 10:31:21 -0400

    add PasswordValidationResponse

---
+++ ApiSites/Roblox.Authentication.Api/Roblox.Authentication.Api/Models/Responses/PasswordValidationResponse.cs
@@ -0,0 +1,8 @@
+namespace Roblox.Authentication.Api.Models
+{
+    public class PasswordValidationResponse
+    {
+        public PasswordValidationStatus code { get; set; }
+        public string message { get; set; }
+    }
+}
