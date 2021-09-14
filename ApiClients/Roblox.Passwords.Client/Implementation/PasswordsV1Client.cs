fa03b0c05dcb6c5c3a6c504f5cdcbe2078b90e72
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-13 21:28:15 -0400

    add error handling;

--- ApiClients/Roblox.Passwords.Client/Implementation/PasswordsV1Client.cs
+++ ApiClients/Roblox.Passwords.Client/Implementation/PasswordsV1Client.cs
@@ -1,8 +1,10 @@
 using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Roblox.ApiClientBase;
+using Roblox.Passwords.Client.Exceptions;
 using Roblox.Passwords.Client.Models.Responses;
 
 namespace Roblox.Passwords.Client
@@ -23,10 +25,22 @@ namespace Roblox.Passwords.Client
                 { "userId", accountId.ToString() },
                 { "password", userProvidedPassword.ToString() },
             };
-            var response = await clientBase.ExecuteHttpRequest("", HttpMethod.Post, null, body, null, null, null,
-                "IsPasswordValid");
-            var parsed = JsonSerializer.Deserialize<ValidatePasswordResponse>(response.body);
-            return parsed.isCorrect;
+            try
+            {
+                var response = await clientBase.ExecuteHttpRequest("", HttpMethod.Post, null, body, null, null, null,
+                    "IsPasswordValid");
+                var parsed = JsonSerializer.Deserialize<ValidatePasswordResponse>(response.body);
+                return parsed.isCorrect;
+            }
+            catch (ApiClientException e)
+            {
+                if (e.statusCode == HttpStatusCode.BadRequest && e.HasError("RecordNotFound"))
+                {
+                    throw new UserHasNoPasswordException();
+                }
+
+                throw;
+            }
         }
     }
 }
