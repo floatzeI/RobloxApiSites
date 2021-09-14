3dbd57bfb43e51d38247b9c9ffe00dea50212a38
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-14 10:55:55 -0400

    add comments about what has to be completed;

--- ApiSites/Roblox.Authentication.Api/Roblox.Authentication.Api/Controllers/V1/SignupController.cs
+++ ApiSites/Roblox.Authentication.Api/Roblox.Authentication.Api/Controllers/V1/SignupController.cs
@@ -45,6 +45,18 @@ namespace Roblox.Authentication.Api.Controllers
         [HttpPost]
         public async Task<Models.SignupResponse> Signup(Models.SignupRequest request)
         {
+            // todo: what has to be called:
+            // 1. Create user with UsersService
+            // 2. Insert locale info for that user
+            // 3. Insert birthday for that user
+            // 4. Insert password for that user
+            // 5. If email specified, insert email record
+            // 6. Create avatar (go off params if specified, otherwise use default specified in AppSettings)
+            // 7. Add default avatar items to inventory, plus items specified in request.assetIds (as long as they're free items)
+            // 8. Create default place and universe for the user
+            // 9. Finally, Create session and set cookie
+            
+            // In the future, we may also want to look into captcha verification (mostly for realism)
             throw new NotImplementedException();
         }
     }
