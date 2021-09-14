5c771f91ddb87525487fca52f1783fcef7fb100e
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-14 15:25:23 -0400

    add GetUserAvatar

--- Services/Roblox.Services/Controllers/V1/AvatarController.cs
+++ Services/Roblox.Services/Controllers/V1/AvatarController.cs
@@ -25,5 +25,18 @@ namespace Roblox.Services.Controllers.V1
         {
             throw new NotImplementedException();
         }
+
+        /// <summary>
+        /// Get the avatar for the userId
+        /// </summary>
+        /// <param name="userId">The user to get the avatar for</param>
+        /// <response code="400">
+        /// RecordNotFound: User does not have an avatar
+        /// </response>
+        [HttpGet("GetUserAvatar")]
+        public async Task<Models.Avatar.AvatarEntry> GetUserAvatar([Required] long userId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
