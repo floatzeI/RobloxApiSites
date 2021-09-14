3c00995a0a2acd58ab8af60df975c7c9fb3d4d95
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-13 21:00:50 -0400

    add IsPasswordValid();

--- Services/Roblox.Services/Controllers/V1/PasswordsController.cs
+++ Services/Roblox.Services/Controllers/V1/PasswordsController.cs
@@ -1,7 +1,36 @@
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Roblox.Services.Services;
+
 namespace Roblox.Services.Controllers.V1
 {
+    [ApiController]
+    [Route("/Passwords/v1/")]
     public class PasswordsController
     {
+        private IPasswordsService service { get; set; }
+        public PasswordsController(IPasswordsService passwordsService)
+        {
+            service = passwordsService;
+        }
         
+        /// <summary>
+        /// Check if the provided password matches the password stored for the accountId
+        /// </summary>
+        /// <param name="request">The request</param>
+        /// <returns>isCorrect will be true if the passwords match, otherwise it will be false</returns>
+        /// <response code="400">
+        /// RecordNotFound: A password entry for this user does not exist
+        /// </response>
+        [HttpPost("IsPasswordValid")]
+        public async Task<Models.Passwords.ValidatePasswordResponse> IsPasswordValid([Required] Models.Passwords.ValidatePasswordRequest request)
+        {
+            var status = await service.IsPasswordCorrectForUser(request.userId, request.password);
+            return new()
+            {
+                isCorrect = status,
+            };
+        }
     }
 }
