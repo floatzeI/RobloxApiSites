7afb6985a5acc5b090e3863ca60f91c8050abe52
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-14 10:30:33 -0400

    add PasswordChangeModel

---
+++ ApiSites/Roblox.Authentication.Api/Roblox.Authentication.Api/Models/Requests/PasswordChangeModel.cs
@@ -0,0 +1,8 @@
+namespace Roblox.Authentication.Api.Models
+{
+    public class PasswordChangeModel
+    {
+        public string currentPassword { get; set; }
+        public string newPassword { get; set; }
+    }
+}
