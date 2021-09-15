b5138cb655d4d6f56a48ca9a8c0b68b6565ea539
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-15 10:29:08 -0400

    add GetUserAvatar

--- Services/Roblox.Services/Services/AvatarService.cs
+++ Services/Roblox.Services/Services/AvatarService.cs
@@ -1,5 +1,8 @@
+using System;
+using System.Threading.Tasks;
 using Roblox.Services.Database;
 using Roblox.Services.DatabaseCache;
+using Roblox.Services.Exceptions.Services;
 
 namespace Roblox.Services.Services
 {
@@ -10,6 +13,36 @@ namespace Roblox.Services.Services
         public AvatarService(IAvatarDatabase db)
         {
             this.db = db;
+        }
+
+        public async Task<Models.Avatar.AvatarEntry> GetUserAvatar(long userId)
+        {
+            var result = await db.GetUserAvatar(userId);
+            if (result == null) throw new RecordNotFoundException();
+            var assets = await db.GetAvatarAssets(userId);
+            return new()
+            {
+                colors = new()
+                {
+                    headColorId = result.headColorId,
+                    torsoColorId = result.torsoColorId,
+                    leftArmColorId = result.leftArmColorId,
+                    rightArmColorId = result.rightArmColorId,
+                    leftLegColorId = result.leftLegColorId,
+                    rightLegColorId = result.rightLegColorId,
+                },
+                scales = new()
+                {
+                    bodyType = result.bodyType,
+                    depth = result.depth,
+                    head = result.head,
+                    height = result.height,
+                    proportion = result.proportion,
+                    width = result.width,
+                },
+                type = result.type,
+                assetIds = assets,
+            };
         }
     }
 }
