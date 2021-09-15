36688cbf916cf3be576ff68fade64cb87f8b3590
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-15 10:28:24 -0400

    add GetUserAvatar

--- Services/Roblox.Services/Interfaces/Services/IAvatarService.cs
+++ Services/Roblox.Services/Interfaces/Services/IAvatarService.cs
@@ -1,7 +1,9 @@
+using System.Threading.Tasks;
+
 namespace Roblox.Services.Services
 {
     public interface IAvatarService
     {
-        
+        Task<Models.Avatar.AvatarEntry> GetUserAvatar(long userId);
     }
 }
