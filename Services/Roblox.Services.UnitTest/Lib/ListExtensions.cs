a7a53a0cea8e3f6ce17afc8c15fc6fc00a45a66f
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-15 11:02:41 -0400

    add GetItemsNotInSecondList();

---
+++ Services/Roblox.Services.UnitTest/Lib/ListExtensions.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Roblox.Services.Lib.Extensions;
+using Roblox.Services.Models;
+using Roblox.Services.Models.Shared;
+using Xunit;
+
+namespace Roblox.Services.UnitTest.Lib
+{
+    public class TestListExtensions
+    {
+        [Fact]
+        public void Get_Longs_Not_In_Second_List()
+        {
+            var firstList = new List<long>()
+            {
+                1,
+                2,
+                3,
+                4,
+            };
+            var secondList = new List<long>()
+            {
+                1,
+                2,
+                4,
+            };
+            var expectedResult = new List<long>()
+            {
+                3,
+            };
+
+            var result = ListExtensions.GetItemsNotInSecondList(firstList, secondList, (l, l1) => l == l1);
+            Assert.Equal(expectedResult.Count, result.Count);
+            for (var i = 0; i < result.Count; i++)
+            {
+                var sibling = expectedResult[i];
+                Assert.Equal(sibling, result[i]);
+            }
+        }
+        
+        [Fact]
+        public void Get_Classes_Not_In_Second_List()
+        {
+            var firstList = new List<Roblox.Services.Models.Shared.HealthCheckResponse>()
+            {
+                new ()
+                {
+                    status = "OK",
+                    name = "Roblox.Testing1.Service",
+                },
+                new ()
+                {
+                    status = "OK",
+                    name = "Roblox.Testing2.Service",
+                },
+                new ()
+                {
+                    status = "OK",
+                    name = "Roblox.Testing3.Service",
+                },
+            };
+            var secondList = new List<HealthCheckResponse>()
+            {
+                new ()
+                {
+                    status = "OK",
+                    name = "Roblox.Testing1.Service",
+                },
+                new ()
+                {
+                    status = "OK",
+                    name = "Roblox.Testing2.Service",
+                },
+            };
+            var expectedResult = new List<HealthCheckResponse>()
+            {
+                new ()
+                {
+                    status = "OK",
+                    name = "Roblox.Testing3.Service",
+                },
+            };
+
+            var result = ListExtensions.GetItemsNotInSecondList(firstList, secondList, (l, l1) => l.name == l1.name && l.status == l1.status);
+            Assert.Equal(expectedResult.Count, result.Count);
+            for (var i = 0; i < result.Count; i++)
+            {
+                var sibling = expectedResult[i];
+                Assert.Equal(sibling.name, result[i].name);
+                Assert.Equal(sibling.status, result[i].status);
+            }
+        }
+        
+        [Fact]
+        public void Get_Empty_Long_List()
+        {
+            var firstList = new List<long>()
+            {
+                1,
+                2,
+                3,
+                4,
+            };
+
+            var result = ListExtensions.GetItemsNotInSecondList(firstList, firstList, (l, l1) => l == l1);
+            Assert.Empty(result);
+        }
+    }
+}
---
+++ Services/Roblox.Services/Lib/ListExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Roblox.Services.Lib.Extensions
+{
+    public static class ListExtensions
+    {
+        /// <summary>
+        /// Get items that appear in the first list, but not in the second
+        /// </summary>
+        /// <param name="first">The first list of T</param>
+        /// <param name="second">The second list of T</param>
+        /// <param name="comparison">A lambda that returns whether both items are equal (true) or not (false)</param>
+        /// <typeparam name="T">The type of the items to compare</typeparam>
+        /// <returns>A new list containing items that do not appear in the second list. Length is 0 if all items appear</returns>
+        public static List<T> GetItemsNotInSecondList<T>(List<T> first, List<T> second, Func<T, T, bool> comparison)
+        {
+            var notInSecondList = new List<T>();
+            foreach (var item in first)
+            {
+                // List<T>.Find() would be nice, but I don't want to force nullable reference types/use default(T) for comparisons
+                var existsInSecondList = false;
+                foreach (var otherItem in second)
+                {
+                    if (!comparison(item, otherItem)) continue;
+                    existsInSecondList = true;
+                    break;
+                }
+                if (!existsInSecondList)
+                {
+                    notInSecondList.Add(item);
+                }
+            }
+
+            return notInSecondList;
+        }
+    }
+}
