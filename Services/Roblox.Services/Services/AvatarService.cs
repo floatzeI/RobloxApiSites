11de68cdac805dd4c870dcdf9d74afb0b90b527a
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-15 11:15:59 -0400

    implement SetUserAvatar;

--- Services/Roblox.Services/Services/AvatarService.cs
+++ Services/Roblox.Services/Services/AvatarService.cs
@@ -1,8 +1,11 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Roblox.Services.Database;
 using Roblox.Services.DatabaseCache;
 using Roblox.Services.Exceptions.Services;
+using Roblox.Services.Lib.Extensions;
 
 namespace Roblox.Services.Services
 {
@@ -43,6 +46,45 @@ namespace Roblox.Services.Services
                 type = result.type,
                 assetIds = assets,
             };
+        }
+
+        public async Task SetUserAvatar(Models.Avatar.SetAvatarRequest request)
+        {
+            var userId = request.userId;
+            var assets = request.assetIds.ToList().Distinct().ToList();
+            // first, get current avatar, figure out the assetIds dif, then insert/remove the assets
+            var current = (await db.GetAvatarAssets(userId)).ToList();
+            var toRemove = ListExtensions.GetItemsNotInSecondList(
+                current, 
+                assets, 
+                (idOne, idTwo) => idOne == idTwo
+            );
+            var toAdd = ListExtensions.GetItemsNotInSecondList(
+                assets, 
+                current, 
+                (idOne, idTwo) => idOne == idTwo
+            );
+
+            // Submit changes
+            foreach (var item in toRemove)
+            {
+                await db.DeleteAvatarAsset(userId, item);
+            }
+
+            foreach (var item in toAdd)
+            {
+                await db.InsertAvatarAsset(userId, item);
+            }
+
+            var hasPreviousAvatar = assets.Count > 0 || await db.GetUserAvatar(userId) != null;
+            if (hasPreviousAvatar)
+            {
+                await db.UpdateUserAvatar(request);
+            }
+            else
+            {
+                await db.InsertUserAvatar(request);
+            }
         }
     }
 }
