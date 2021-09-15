676b2f0930334a510cbc23b4a511c51b30541491
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-15 10:28:32 -0400

    add GetUserAvatar; add GetAvatarAssets

--- Services/Roblox.Services/Interfaces/Database/IAvatarDatabase.cs
+++ Services/Roblox.Services/Interfaces/Database/IAvatarDatabase.cs
@@ -1,9 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Roblox.Services.DatabaseCache;
 
 namespace Roblox.Services.Database
 {
     public interface IAvatarDatabase
     {
-        
+        Task<Models.Avatar.DbAvatarEntry> GetUserAvatar(long userId);
+        Task<IEnumerable<long>> GetAvatarAssets(long userId);
     }
 }
