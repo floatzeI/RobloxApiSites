45f2f969855926f5900c56024473080e8bb2590f
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-15 11:42:50 -0400

    add docs for ExecuteHttpRequest

--- ApiClients/ApiClientBase/Roblox.ApiClientBase/Interfaces/IGuardedApiClientBase.cs
+++ ApiClients/ApiClientBase/Roblox.ApiClientBase/Interfaces/IGuardedApiClientBase.cs
@@ -12,6 +12,19 @@ namespace Roblox.ApiClientBase
         void OnRequestFailed(ApiResponseException ex, long requestId, string actionName,
             int? statusCode, string statusDescription, string actionPath);
 
+        /// <summary>
+        /// Make an HTTP Request
+        /// </summary>
+        /// <param name="actionPath"></param>
+        /// <param name="method"></param>
+        /// <param name="queryStringParameters"></param>
+        /// <param name="formParameters"></param>
+        /// <param name="headers"></param>
+        /// <param name="rawPostData"></param>
+        /// <param name="multipartFormData"></param>
+        /// <param name="actionName"></param>
+        /// <returns>The <see cref="ApiClientResponse"/></returns>
+        /// <exception cref="ApiClientException">Api return non-200 code</exception>
         Task<ApiClientResponse> ExecuteHttpRequest(string actionPath, HttpMethod method,
             Dictionary<string, string> queryStringParameters,
             Dictionary<string, string> formParameters, Dictionary<string, string> headers, string rawPostData,
