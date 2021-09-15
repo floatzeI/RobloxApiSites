5c70bd691e36267012ed143b46fdd8638267fdff
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-15 17:24:59 -0400

    add assetId property

--- Services/Roblox.Services/Models/Assets/UpdateAsset.cs
+++ Services/Roblox.Services/Models/Assets/UpdateAsset.cs
@@ -2,6 +2,7 @@ namespace Roblox.Services.Models.Assets
 {
     public class UpdateAssetRequest
     {
+        public long assetId { get; set; }
         public string name { get; set; }
         public string description { get; set; }
         public int creatorType { get; set; }
