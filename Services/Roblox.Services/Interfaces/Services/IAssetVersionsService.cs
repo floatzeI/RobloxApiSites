ce66e24e8a7f537d2e069f01e77a52192cdfef98
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-16 15:53:23 -0400

    add IAssetVersionsService

---
+++ Services/Roblox.Services/Interfaces/Services/IAssetVersionsService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace Roblox.Services.Services
+{
+    public interface IAssetVersionsService
+    {
+        Task<Models.AssetVersions.AssetVersionEntry> InsertAssetVersion(Models.AssetVersions.InsertAssetVersionRequest request);
+        Task DeleteAssetVersion(long assetVersionId);
+        Task<Models.AssetVersions.AssetVersionEntry> GetLatestAssetVersion(long assetId);
+    }
+}
