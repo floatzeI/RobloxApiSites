7ba35dd5932b8966a125074b9a70f7e7212d8e27
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-16 11:18:33 -0400

    implement IFilesDatabase;

--- Services/Roblox.Services/Database/FilesDatabase.cs
+++ Services/Roblox.Services/Database/FilesDatabase.cs
@@ -1,4 +1,8 @@
-﻿namespace Roblox.Services.Database
+﻿using System.IO;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace Roblox.Services.Database
 {
     public class FilesDatabase : IFilesDatabase
     {
@@ -7,6 +11,26 @@
         public FilesDatabase(DatabaseConfiguration<dynamic> config)
         {
             db = config.dbConnection;
+        }
+
+        public async Task InsertFile(string fileHash, string mimeType, long sizeInBytes)
+        {
+            await db.connection.ExecuteAsync("INSERT INTO file (id, mime, size_bytes) VALUES (@id, @mime, @size_bytes)",
+                new
+                {
+                    id = fileHash,
+                    mime = mimeType,
+                    size_bytes = sizeInBytes,
+                });
+        }
+
+        public async Task<bool> DoesFileExist(string fileHash)
+        {
+            var exists = await db.connection.QuerySingleOrDefaultAsync("SELECT id FROM file WHERE id = @id", new
+            {
+                id = fileHash,
+            });
+            return exists != null;
         }
     }
 }
