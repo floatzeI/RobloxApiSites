0081dec359cd06bc4edb09a80a6871ae869c0034
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-16 16:11:00 -0400

    add insert two test case; inherit integrationtestbase;

--- Services/Roblox.Services.IntegrationTest/Controllers/AssetVersionsController.cs
+++ Services/Roblox.Services.IntegrationTest/Controllers/AssetVersionsController.cs
@@ -9,7 +9,7 @@ using Xunit;
 
 namespace Roblox.Services.IntegrationTest.Controllers.V1
 {
-    public class IntegrationTestAssetVersionsController
+    public class IntegrationTestAssetVersionsController : IntegrationTestBase
     {
         [Fact]
         public async Task Add_Asset_Version()
@@ -28,7 +28,28 @@ namespace Roblox.Services.IntegrationTest.Controllers.V1
             var response = await controller.InsertAssetVersion(request);
             Assert.Equal(1, response.versionNumber);
             Assert.Equal(123, response.assetId);
-            Assert.True(response.versionNumber == 1);
+        }
+        
+        [Fact]
+        public async Task Add_Two_Asset_Versions()
+        {
+            // string "test"
+            var hash =
+                "EE26B0DD4AF7E749AA1A8EE3C10AE9923F618980772E473F8819A5D4940E0DB27AC185F8A0E1D5F84F88BC887FD67B143732C304CC5FA9AD8E6F57F50028A8FF";
+            var request = new InsertAssetVersionRequest()
+            {
+                assetId = 1234,
+                fileId = hash,
+                userId = 1,
+            };
+            var controller = new AssetVersionsController(new AssetVersionsService(
+                new AssetVersionsDatabase(new(new PostgresDatabaseProvider(), null))));
+            // insert once
+            await controller.InsertAssetVersion(request);
+            // now do it again
+            var response = await controller.InsertAssetVersion(request);
+            Assert.Equal(2, response.versionNumber);
+            Assert.Equal(1234, response.assetId);
         }
     }
 }
