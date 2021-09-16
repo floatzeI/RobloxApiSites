119c0ee258a6f329da63283fab6a87896885fbcd
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-16 15:52:59 -0400

    add asset_version table

---
+++ Services/Roblox.Services/Migrations/sql/2021-09-16 18-57-40_addAssetVersionsTable.js
@@ -0,0 +1,27 @@
+/**
+ * @param {import('pg').Client} conn
+ */
+exports.up = async (conn) => {
+    await conn.query(`
+
+CREATE TABLE asset_version (
+    id bigserial NOT NULL, -- asset version id
+    file_id varchar(128) NOT NULL, -- ID in Roblox.Files.Service
+    asset_id bigint NOT NULL,
+    user_id bigint NOT NULL, -- The userId that uploaded this version. For instance, in a group team create session, this would be the user who clicked "Publish"
+    version_number int NOT NULL, -- The version number of the item
+    created_at timestamp DEFAULT(CURRENT_TIMESTAMP) NOT NULL
+);
+
+-- For queries that want the latest version for a specific assetId (which will be common)
+create index asset_version_asset_id_created_at_desc on asset_version(asset_id, created_at DESC);
+
+`);
+}
+
+/**
+ * @param {import('pg').Client} conn
+ */
+exports.down = async (conn) => {
+    await conn.query(`DROP TABLE asset_version`);
+}
