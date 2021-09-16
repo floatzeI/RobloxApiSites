0bc270c5eae6a44c86e8fbaa3fce3437c1bf1be4
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-16 11:17:14 -0400

    add CreateFileHash(); add UploadFile();

--- Services/Roblox.Services/Interfaces/Services/IFilesService.cs
+++ Services/Roblox.Services/Interfaces/Services/IFilesService.cs
@@ -1,7 +1,12 @@
-﻿namespace Roblox.Services.Services
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace Roblox.Services.Services
 {
     public interface IFilesService
     {
-        
+        Task<string> CreateFileHash(Stream fileStream);
+
+        Task UploadFile(Stream fileStream, string fileHash, string mimeOverride);
     }
 }
