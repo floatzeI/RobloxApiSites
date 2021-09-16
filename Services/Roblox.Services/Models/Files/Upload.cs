f2df52e82bbc0b9330c939ec13ea7828756355b0
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-16 11:16:45 -0400

    add UploadRequest; add UploadResponse

---
+++ Services/Roblox.Services/Models/Files/Upload.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+namespace Roblox.Services.Models.Files
+{
+    public class UploadRequest
+    {
+        /// <summary>
+        /// The file mime type. Defaults to null.
+        /// </summary>
+        public string mime { get; set; }
+        /// <summary>
+        /// The file to upload
+        /// </summary>
+        [Required]
+        public IFormFile file { get; set; }
+    }
+
+    public class UploadResponse
+    {
+        /// <summary>
+        /// The generated fileId. This should be a hash, up to 64 characters.
+        /// </summary>
+        public string id { get; set; }
+    }
+}
