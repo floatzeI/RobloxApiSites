deb9c74572b19fcb4d37524a2204e633fd4ac72b
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-16 11:17:50 -0400

    add InsertFile(); add DoesFileExist();

--- Services/Roblox.Services/Interfaces/Database/IFilesDatabase.cs
+++ Services/Roblox.Services/Interfaces/Database/IFilesDatabase.cs
@@ -1,7 +1,11 @@
-﻿namespace Roblox.Services.Database
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace Roblox.Services.Database
 {
     public interface IFilesDatabase
     {
-        
+        Task InsertFile(string fileHash, string mimeType, long sizeInBytes);
+        Task<bool> DoesFileExist(string fileHash);
     }
 }
