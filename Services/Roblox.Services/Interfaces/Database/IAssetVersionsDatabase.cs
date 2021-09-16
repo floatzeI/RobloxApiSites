fc435bdcf813eff7d9bd4f6f1ff37e9b2ee1b984
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-16 15:53:28 -0400

    add IAssetVersionsDatabase

---
+++ Services/Roblox.Services/Interfaces/Database/IAssetVersionsDatabase.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Roblox.Services.Models.AssetVersions;
+
+namespace Roblox.Services.Database
+{
+    public interface IAssetVersionsDatabase
+    {
+        Task<AssetVersionEntry> InsertAssetVersion(InsertAssetVersionRequest request);
+        Task DeleteAssetVersion(long assetVersionId);
+        Task<AssetVersionEntry> GetLatestAssetVersion(long assetId);
+    }
+}
