d182a4d3cbcdaf5efa88ab2351010400153154ff
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-16 15:53:48 -0400

    add AssetVersionsController; add health check;

---
+++ Services/Roblox.Services/Controllers/AssetVersionsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Roblox.Services.Controllers
+{
+    [ApiController]
+    [Route("/AssetVersions")]
+    public class AssetVersionsController
+    {
+        [HttpGet]
+        public Models.Shared.HealthCheckResponse GetStatus()
+        {
+            return new()
+            {
+                name = "Roblox.AssetVersions.Storage",
+                status = "OK"
+            };
+        }
+    }
+}
