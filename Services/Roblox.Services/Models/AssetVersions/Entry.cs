0e7ea8a88c5a9a3a2561b3cbe7b73e4172901679
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-16 15:53:04 -0400

    add AssetVersionEntry

---
+++ Services/Roblox.Services/Models/AssetVersions/Entry.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Roblox.Services.Models.AssetVersions
+{
+    public class AssetVersionEntry
+    {
+        public long assetVersionId { get; set; }
+        public long assetId { get; set; }
+        /// <summary>
+        /// The userId who created the version
+        /// </summary>
+        public long userId { get; set; }
+        /// <summary>
+        /// The version of the item, incrementing each time a new asset version is inserted for the assetId
+        /// </summary>
+        public int versionNumber { get; set; }
+        /// <summary>
+        /// ID of the file in Roblox.Files.Service
+        /// </summary>
+        /// <remarks>128 characters long, hex</remarks>
+        public string fileId { get; set; }
+        public DateTime created { get; set; }
+    }
+}
