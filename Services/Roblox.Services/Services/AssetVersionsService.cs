9ce3b30dff49543fcf2b699a812bca5b33b5b513
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-16 15:53:11 -0400

    implement IAssetVersionsService;

---
+++ Services/Roblox.Services/Services/AssetVersionsService.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using Roblox.Services.Database;
+using Roblox.Services.Exceptions.Services;
+using Roblox.Services.Models.AssetVersions;
+
+namespace Roblox.Services.Services
+{
+    public class AssetVersionsService : IAssetVersionsService
+    {
+        private IAssetVersionsDatabase db { get; set; }
+
+        public AssetVersionsService(IAssetVersionsDatabase assetVersionsDatabase)
+        {
+            db = assetVersionsDatabase;
+        }
+
+
+        public async Task<AssetVersionEntry> InsertAssetVersion(InsertAssetVersionRequest request)
+        {
+            return await db.InsertAssetVersion(request);
+        }
+
+        public async Task DeleteAssetVersion(long assetVersionId)
+        {
+            await db.DeleteAssetVersion(assetVersionId);
+        }
+
+        public async Task<AssetVersionEntry> GetLatestAssetVersion(long assetId)
+        {
+            var result = await db.GetLatestAssetVersion(assetId);
+            if (result == null) throw new RecordNotFoundException(assetId);
+            return result;
+        }
+    }
+}
