bfa3351a412b519e4e9f49c4cb94ff1eee94517f
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-16 15:53:39 -0400

    add AssetVersionsController

---
+++ Services/Roblox.Services/Controllers/V1/AssetVersionsController.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Roblox.Services.Models.AssetVersions;
+using Roblox.Services.Services;
+
+namespace Roblox.Services.Controllers.V1
+{
+    [ApiController]
+    [Route("/AssetVersions/v1")]
+    public class AssetVersionsController
+    {
+        private IAssetVersionsService assetVersionsService { get; set; }
+
+        public AssetVersionsController(IAssetVersionsService service)
+        {
+            assetVersionsService = service;
+        }
+
+        [HttpPost("AddAssetVersion")]
+        public async Task<AssetVersionEntry> InsertAssetVersion([FromBody, Required] InsertAssetVersionRequest request)
+        {
+            return await assetVersionsService.InsertAssetVersion(request);
+        }
+        
+        [HttpPost("DeleteAssetVersion")]
+        public async Task InsertAssetVersion([Required] long assetVersionId)
+        {
+            await assetVersionsService.DeleteAssetVersion(assetVersionId);
+        }
+        
+        [HttpPost("GetLatestAssetVersion")]
+        public async Task<AssetVersionEntry> GetLatestAssetVersion([Required] long assetId)
+        {
+            return await assetVersionsService.GetLatestAssetVersion(assetId);
+        }
+    }
+}
