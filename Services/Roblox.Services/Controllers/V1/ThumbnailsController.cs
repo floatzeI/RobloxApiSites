ac1c23da4bf794a64969f8c1556c4903ab8edf36
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-17 16:39:31 -0400

    add ThumbnailsController v1; add GetThumbnail(); add InsertThumbnail();

---
+++ Services/Roblox.Services/Controllers/V1/ThumbnailsController.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Roblox.Services.Services;
+
+namespace Roblox.Services.Controllers.V1
+{
+    [ApiController]
+    [Route("/Thumbnails/v1")]
+    public class ThumbnailsController
+    {
+        private IThumbnailsService thumbnailsService { get; set; }
+
+        public ThumbnailsController(IThumbnailsService service)
+        {
+            thumbnailsService = service;
+        }
+        
+        [HttpGet("GetThumbnail")]
+        public async Task<Models.Thumbnails.ThumbnailEntry> GetThumbnail([Required] long referenceId, [Required] int thumbnailType,
+            [Required] int resolutionX, [Required] int resolutionY)
+        {
+            return await thumbnailsService.GetThumbnail(referenceId, thumbnailType, resolutionX, resolutionY);
+        }
+
+        [HttpPost("InsertThumbnail")]
+        public async Task InsertThumbnail([Required, FromBody] Models.Thumbnails.ThumbnailEntry request)
+        {
+            await thumbnailsService.InsertThumbnail(request);
+        }
+    }
+}
