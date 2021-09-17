0260f13cdd5c60f7139c6ca279bb065e4b9c2576
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-17 16:26:26 -0400

    add IThumbnailsDatabase

---
+++ Services/Roblox.Services/Interfaces/Database/IThumbnailsDatabase.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace Roblox.Services.Database
+{
+    public interface IThumbnailsDatabase
+    {
+        Task InsertThumbnail(Models.Thumbnails.ThumbnailEntry request);
+        Task<Models.Thumbnails.ThumbnailEntry> GetThumbnail(long referenceId, int thumbnailType);
+
+        Task<Models.Thumbnails.ThumbnailEntry> GetThumbnail(long referenceId, int thumbnailType, int resolutionX,
+            int resolutionY);
+
+        Task<Models.Thumbnails.ThumbnailEntry> ResolveThumbnailHash(string thumbnailHash, int resolutionX,
+            int resolutionY);
+    }
+}
