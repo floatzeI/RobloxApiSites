77435f090927cdcb93a589320df422c5d4c40dbb
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-16 21:03:51 -0400

    add IThumbnailManager

---
+++ Platform/Thumbnail/Roblox.Platform.Thumbnail/Interfaces/IThumbnailManager.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Roblox.Platform.Thumbnail.Models;
+
+namespace Roblox.Platform.Thumbnail
+{
+    public interface IThumbnailManager
+    {
+        /// <summary>
+        /// Upload a PNG thumbnail
+        /// </summary>
+        /// <param name="request">The upload request</param>
+        Task UploadPngThumbnail(ThumbnailUploadRequest request);
+    }
+}
