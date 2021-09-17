1381cc286f751c398991667c19a7ecb757a730b9
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-16 21:04:01 -0400

    add ThumbnailUploadRequest

---
+++ Platform/Thumbnail/Roblox.Platform.Thumbnail/Models/Requests/ThumbnailUploadRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+namespace Roblox.Platform.Thumbnail.Models
+{
+    public class ThumbnailUploadRequest
+    {
+        /// <summary>
+        /// A unique identifier. It must be 32 characters and in hex format.
+        /// </summary>
+        /// <remarks>
+        /// Some examples could include a hash of the data required to make the thumbnail (e.g. an md5 hash of a json object representing a player thumbnail), or the md5 hash of the image itself
+        /// </remarks>
+        public string id { get; set; }
+        public Stream file { get; set; }
+        public string mime { get; set; }
+        public ThumbnailType thumbnailType { get; set; }
+    }
+}
