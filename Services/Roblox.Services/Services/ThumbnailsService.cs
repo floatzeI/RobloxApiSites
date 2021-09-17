6c85d3facda9ebedaec088b954c84b96bf0bbde4
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-17 16:26:13 -0400

    implement IThumbnailsService;

---
+++ Services/Roblox.Services/Services/ThumbnailsService.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading.Tasks;
+using Roblox.Services.Database;
+using Roblox.Services.Exceptions.Services;
+using Roblox.Services.Models.Thumbnails;
+
+namespace Roblox.Services.Services
+{
+    public class ThumbnailsService : IThumbnailsService
+    {
+        private IThumbnailsDatabase thumbnailsDatabase { get; set; }
+
+        public ThumbnailsService(IThumbnailsDatabase db)
+        {
+            thumbnailsDatabase = db;
+        }
+
+        public async Task InsertThumbnail(ThumbnailEntry request)
+        {
+            await thumbnailsDatabase.InsertThumbnail(request);
+        }
+
+        public async Task<ThumbnailEntry> GetThumbnail(long referenceId, int thumbnailType)
+        {
+            var result = await thumbnailsDatabase.GetThumbnail(referenceId, thumbnailType);
+            if (result == null) throw new RecordNotFoundException(referenceId);
+            return result;
+        }
+
+        public async Task<ThumbnailEntry> GetThumbnail(long referenceId, int thumbnailType, int resolutionX, int resolutionY)
+        {
+            var result = await thumbnailsDatabase.GetThumbnail(referenceId, thumbnailType, resolutionX, resolutionY);
+            if (result == null) throw new RecordNotFoundException(referenceId);
+            return result;
+        }
+
+        public async Task<ThumbnailEntry> GetThumbnailByHash(string thumbnailHash, int resolutionX, int resolutionY)
+        {
+            var result = await thumbnailsDatabase.ResolveThumbnailHash(thumbnailHash, resolutionX, resolutionY);
+            if (result == null) throw new RecordNotFoundException();
+            return result;
+        }
+    }
+}
