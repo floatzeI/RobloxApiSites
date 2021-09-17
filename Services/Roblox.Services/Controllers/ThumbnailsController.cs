ddcec325240eabbf1e5dbb22c5408c86c7aadb55
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-17 16:26:38 -0400

    add GetStatus()

---
+++ Services/Roblox.Services/Controllers/ThumbnailsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Roblox.Services.Controllers
+{
+    [ApiController]
+    [Route("/Thumbnails")]
+    public class ThumbnailsController
+    {
+        [HttpGet]
+        public Models.Shared.HealthCheckResponse GetStatus()
+        {
+            return new()
+            {
+                name = "Roblox.Thumbnails.Service",
+                status = "OK"
+            };
+        }
+    }
+}
