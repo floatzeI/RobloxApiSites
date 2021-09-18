081c4d973e78eb626ace6e729bad87ed1164c2f6
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-18 17:05:36 -0400

    add IMarketplaceDatabase

---
+++ Services/Roblox.Services/Interfaces/Database/IMarketplaceDatabase.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace Roblox.Services.Database
+{
+    public interface IMarketplaceDatabase
+    {
+        Task<Models.Marketplace.AssetEntry> GetProduct(long productId);
+        Task<Models.Marketplace.AssetEntry> GetProductByAssetId(long assetId);
+        Task<long> InsertProduct(Models.Marketplace.AssetEntry request);
+        Task UpdateProduct(Models.Marketplace.AssetEntry request);
+    }
+}
