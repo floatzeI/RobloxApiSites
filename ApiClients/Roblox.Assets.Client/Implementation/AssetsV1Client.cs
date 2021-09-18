6ea258bbba54c4c1c0ca6cc06225eb49c5c3fecd
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-18 18:54:17 -0400

    implement CreateAsset(); implement SetAssetGenres();

--- ApiClients/Roblox.Assets.Client/Implementation/AssetsV1Client.cs
+++ ApiClients/Roblox.Assets.Client/Implementation/AssetsV1Client.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
@@ -6,6 +7,7 @@ using System.Threading.Tasks;
 using Roblox.ApiClientBase;
 using Roblox.Assets.Client.Exceptions;
 using Roblox.Assets.Client.Models;
+using Roblox.Web.Enums;
 
 namespace Roblox.Assets.Client
 {
@@ -35,6 +37,22 @@ namespace Roblox.Assets.Client
             var result =
                 await clientBase.ExecuteHttpRequest("", HttpMethod.Get, query, null, null, null, null, "MultiGetDetails");
             return JsonSerializer.Deserialize<IEnumerable<Models.AssetDetailsEntry>>(result.body);
+        }
+
+        public async Task<Models.CreateAssetResponse> CreateAsset(Models.CreateAssetRequest request)
+        {
+            var result = await clientBase.ExecuteHttpRequest("", HttpMethod.Post, null, null, null,
+                JsonSerializer.Serialize(request), null, "InsertAsset");
+            return JsonSerializer.Deserialize<CreateAssetResponse>(result.body);
+        }
+
+        public async Task SetAssetGenres(long assetId, IEnumerable<AssetGenre> genres)
+        {
+            await clientBase.ExecuteHttpRequest("", HttpMethod.Post, null, null, null, JsonSerializer.Serialize(new
+            {
+                assetId = assetId,
+                genres = genres,
+            }), null, "SetAssetGenres");
         }
     }
 }
