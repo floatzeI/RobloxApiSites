dd67f340e399075a4c6a21d82a3c163cacb32fcf
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-18 17:05:40 -0400

    add IMarketplaceService

---
+++ Services/Roblox.Services/Interfaces/Services/IMarketplaceService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace Roblox.Services.Services
+{
+    public interface IMarketplaceService
+    {
+        Task<Models.Marketplace.AssetEntry> GetProduct(long productId);
+        Task<Models.Marketplace.AssetEntry> GetProductByAssetId(long assetId);
+        Task<Models.Marketplace.InsertResponse> SetProductForAssetId(Models.Marketplace.AssetEntry request);
+    }
+}
