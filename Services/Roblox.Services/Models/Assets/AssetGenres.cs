3801c5a00565f9d4d618f5b856b065562d91f6f8
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-18 10:30:17 -0400

    add UpdateAssetGenresRequest

---
+++ Services/Roblox.Services/Models/Assets/AssetGenres.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Roblox.Services.Models.Assets
+{
+    public class UpdateAssetGenresRequest
+    {
+        public long assetId { get; set; }
+        public IEnumerable<int> genres { get; set; }
+    }
+}
