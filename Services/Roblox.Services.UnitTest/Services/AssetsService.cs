e763521bbb0dac985f0693520479554801bc7004
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-18 10:47:28 -0400

    add get/set genres unit tests

--- Services/Roblox.Services.UnitTest/Services/AssetsService.cs
+++ Services/Roblox.Services.UnitTest/Services/AssetsService.cs
@@ -153,5 +153,70 @@ namespace Roblox.Services.UnitTest.Services
             await service.UpdateAsset(updateRequest);
             mock.Verify(c => c.UpdateAsset(updateRequest), Times.Once);
         }
+
+        [Fact]
+        public async Task Get_Asset_Genres()
+        {
+            var assetId = 123;
+            var mock = new Mock<IAssetsDatabase>();
+            mock.Setup(c => c.GetAssetGenres(assetId)).ReturnsAsync(new List<int>() { 1, 2, 3 });
+            var service = new AssetsService(mock.Object);
+            var result = await service.GetAssetGenres(assetId);
+            Assert.Equal(3, result.Count());
+            mock.Verify(c => c.GetAssetGenres(assetId), Times.Once);
+        }
+        
+        [Fact]
+        public async Task Set_Asset_Genres_No_Previous()
+        {
+            var assetId = 123;
+            var genres = new List<int>()
+            {
+                1, 2, 3
+            };
+            var mock = new Mock<IAssetsDatabase>();
+            mock.Setup(c => c.GetAssetGenres(assetId)).ReturnsAsync(new int[] { });
+            var service = new AssetsService(mock.Object);
+            await service.SetAssetGenres(assetId, genres);
+            mock.Verify(c => c.GetAssetGenres(assetId), Times.Once);
+            mock.Verify(c => c.InsertAssetGenres(assetId, It.IsAny<IEnumerable<int>>()), Times.Once);
+            mock.Verify(c => c.DeleteAssetGenres(assetId, It.IsAny<IEnumerable<int>>()), Times.Never);
+        }
+                
+        [Fact]
+        public async Task Set_Asset_Genres_With_Previous()
+        {
+            var assetId = 123;
+            var genres = new List<int>()
+            {
+                1, 3, 4 // adding ID=4, deleting ID=2
+            };
+            var mock = new Mock<IAssetsDatabase>();
+            mock.Setup(c => c.GetAssetGenres(assetId)).ReturnsAsync(new int[]
+            {
+                1,
+                2,
+                3,
+            });
+            mock.Setup(c => c.InsertAssetGenres(assetId, It.IsAny<IEnumerable<int>>())).Callback<long, IEnumerable<int>>((id, genres) =>
+            {
+                var ids = genres as int[] ?? genres.ToArray();
+                Assert.Single(ids);
+                Assert.Equal(4, ids[0]);
+                Assert.Equal(assetId, id);
+            });
+            mock.Setup(c => c.DeleteAssetGenres(assetId, It.IsAny<IEnumerable<int>>())).Callback<long, IEnumerable<int>>((id, genres) =>
+            {
+                var ids = genres as int[] ?? genres.ToArray();
+                Assert.Single(ids);
+                Assert.Equal(2, ids[0]);
+                Assert.Equal(assetId, id);
+            });
+            var service = new AssetsService(mock.Object);
+            await service.SetAssetGenres(assetId, genres);
+            mock.Verify(c => c.GetAssetGenres(assetId), Times.Once);
+            mock.Verify(c => c.InsertAssetGenres(assetId, It.IsAny<IEnumerable<int>>()), Times.Once);
+            mock.Verify(c => c.DeleteAssetGenres(assetId, It.IsAny<IEnumerable<int>>()), Times.Once);
+        }
     }
 }
