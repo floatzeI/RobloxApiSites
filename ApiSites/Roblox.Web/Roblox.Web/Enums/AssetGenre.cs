235d3dd8ad71feda20ff3c96cdde01fb37db2775
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-18 18:33:50 -0400

    add AssetGenre

---
+++ ApiSites/Roblox.Web/Roblox.Web/Enums/AssetGenre.cs
@@ -0,0 +1,20 @@
+﻿namespace Roblox.Web.Enums
+{
+    public enum AssetGenre
+    {
+        All = 0,
+        TownAndCity = 1,
+        Fantasy = 2,
+        SciFi = 3,
+        Ninja = 4,
+        Scary = 5,
+        Pirate = 6,
+        Adventure = 7,
+        Sports = 8,
+        Funny = 9,
+        WildWest = 10,
+        War = 11,
+        SkatePark = 12,
+        Tutorial = 13,
+    }
+}
