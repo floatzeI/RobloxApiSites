0d20276a30daa9aa4d628149d6140fe8d659eb02
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-18 17:17:54 -0400

    add marketplace unit tests

---
+++ Services/Roblox.Services.UnitTest/Controllers/MarketplaceController.cs
@@ -0,0 +1,52 @@
+﻿using System.Threading.Tasks;
+using Moq;
+using Roblox.Services.Controllers.V1;
+using Roblox.Services.Models.Marketplace;
+using Roblox.Services.Services;
+using Xunit;
+
+namespace Roblox.Services.UnitTest.Controllers
+{
+    public class IntegrationTestMarketplaceController
+    {
+        [Fact]
+        public async Task Get_Product_By_Id()
+        {
+            var id = 123;
+            var mock = new Mock<IMarketplaceService>();
+
+            var controller = new MarketplaceController(mock.Object);
+            await controller.GetProductByProductId(id);
+            
+            mock.Verify(c => c.GetProduct(id), Times.Once);
+        }
+        
+        [Fact]
+        public async Task Get_Product_By_Asset_Id()
+        {
+            var assetId = 356;
+            var mock = new Mock<IMarketplaceService>();
+
+            var controller = new MarketplaceController(mock.Object);
+            await controller.GetProductByAssetId(assetId);
+            
+            mock.Verify(c => c.GetProductByAssetId(assetId), Times.Once);
+        }
+                
+        [Fact]
+        public async Task Set_Product_For_Asset_Id()
+        {
+            var request = new AssetEntry()
+            {
+                assetId = 123,
+                productId = 124356,
+            };
+            var mock = new Mock<IMarketplaceService>();
+
+            var controller = new MarketplaceController(mock.Object);
+            await controller.UpdateProduct(request);
+            
+            mock.Verify(c => c.SetProductForAssetId(request), Times.Once);
+        }
+    }
+}
---
+++ Services/Roblox.Services.UnitTest/Services/MarketplaceService.cs
@@ -0,0 +1,116 @@
+﻿using System.Threading.Tasks;
+using Moq;
+using Roblox.Services.Database;
+using Roblox.Services.Exceptions.Services;
+using Roblox.Services.Models.Marketplace;
+using Roblox.Services.Services;
+using Xunit;
+
+namespace Roblox.Services.UnitTest.Services
+{
+    public class UnitTestMarketplaceService
+    {
+        [Fact]
+        public async Task Get_Product_By_Id()
+        {
+            var id = 123;
+            var mock = new Mock<IMarketplaceDatabase>();
+            var response = new AssetEntry()
+            {
+                productId = id,
+                assetId = 123,
+            };
+            mock.Setup(c => c.GetProduct(id)).ReturnsAsync(response);
+            var service = new MarketplaceService(mock.Object);
+            var resp = await service.GetProduct(id);
+            Assert.Equal(response, resp);
+        }
+        
+        [Fact]
+        public async Task Get_Product_By_Id_Not_Exists()
+        {
+            var id = 457;
+            var mock = new Mock<IMarketplaceDatabase>();
+            mock.Setup(c => c.GetProduct(id)).ReturnsAsync((AssetEntry)null);
+
+            var service = new MarketplaceService(mock.Object);
+            await Assert.ThrowsAsync<RecordNotFoundException>(async () =>
+            {
+                await service.GetProduct(id);
+            });
+        }
+
+        [Fact]
+        public async Task Get_Product_By_Asset_Id()
+        {
+            var assetId = 123;
+            var mock = new Mock<IMarketplaceDatabase>();
+            mock.Setup(c => c.GetProductByAssetId(assetId)).ReturnsAsync(new AssetEntry()
+            {
+                productId = 12354,
+                assetId = assetId,
+            });
+
+            var service = new MarketplaceService(mock.Object);
+            var result = await service.GetProductByAssetId(assetId);
+            Assert.Equal(assetId, result.assetId);
+            Assert.Equal(12354, result.productId);
+        }
+        
+        [Fact]
+        public async Task Get_Product_By_Asset_Id_Not_Exists()
+        {
+            var assetId = 123;
+            var mock = new Mock<IMarketplaceDatabase>();
+            mock.Setup(c => c.GetProductByAssetId(assetId)).ReturnsAsync((AssetEntry)null);
+
+            var service = new MarketplaceService(mock.Object);
+            await Assert.ThrowsAsync<RecordNotFoundException>(async () =>
+            {
+                await service.GetProductByAssetId(assetId);
+            });
+        }
+
+        [Fact]
+        public async Task Set_Product_No_Previous_Entry()
+        {
+            var request = new AssetEntry()
+            {
+                assetId = 123,
+                productId = 456,
+            };
+            var mock = new Mock<IMarketplaceDatabase>();
+            mock.Setup(c => c.GetProductByAssetId(request.assetId)).ReturnsAsync((AssetEntry)null);
+            mock.Setup(c => c.InsertProduct(request)).ReturnsAsync(69420);
+
+            var service = new MarketplaceService(mock.Object);
+            var result = await service.SetProductForAssetId(request);
+            Assert.Equal(69420, result.productId);
+            
+            mock.Verify(c => c.UpdateProduct(request), Times.Never);
+            mock.Verify(c => c.InsertProduct(request), Times.Once);
+        }
+        
+        [Fact]
+        public async Task Set_Product_With_Previous_Entry()
+        {
+            var request = new AssetEntry()
+            {
+                assetId = 123,
+                productId = 456,
+            };
+            var mock = new Mock<IMarketplaceDatabase>();
+            mock.Setup(c => c.GetProductByAssetId(request.assetId)).ReturnsAsync(new AssetEntry()
+            {
+                productId = 43621,
+            });
+
+            var service = new MarketplaceService(mock.Object);
+            var result = await service.SetProductForAssetId(request);
+            Assert.Equal(43621, result.productId);
+            
+            mock.Verify(c => c.InsertProduct(request), Times.Never);
+            mock.Verify(c => c.UpdateProduct(request), Times.Once);
+        }
+    }
+}
