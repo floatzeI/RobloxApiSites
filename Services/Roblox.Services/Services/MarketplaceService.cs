371e24e8f60e272e26b3b9167ffc2934270f9de0
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-18 17:05:46 -0400

    implement IMarketplaceService;

---
+++ Services/Roblox.Services/Services/MarketplaceService.cs
@@ -0,0 +1,51 @@
+﻿using System.Threading.Tasks;
+using Roblox.Services.Database;
+using Roblox.Services.Exceptions.Services;
+using Roblox.Services.Models.Marketplace;
+
+namespace Roblox.Services.Services
+{
+    public class MarketplaceService : IMarketplaceService
+    {
+        private IMarketplaceDatabase db { get; set; }
+
+        public MarketplaceService(IMarketplaceDatabase db)
+        {
+            this.db = db;
+        }
+
+        public async Task<AssetEntry> GetProduct(long productId)
+        {
+            var response = await db.GetProduct(productId);
+            if (response == null) throw new RecordNotFoundException(productId);
+            return response;
+        }
+
+        public async Task<AssetEntry> GetProductByAssetId(long assetId)
+        {
+            var response = await db.GetProductByAssetId(assetId);
+            if (response == null) throw new RecordNotFoundException(assetId);
+            return response;
+        }
+
+        public async Task<InsertResponse> SetProductForAssetId(AssetEntry request)
+        {
+            var existingProduct = await db.GetProductByAssetId(request.assetId);
+            if (existingProduct != null)
+            {
+                request.productId = existingProduct.productId;
+                await db.UpdateProduct(request);
+                return new()
+                {
+                    productId = request.productId,
+                };
+            }
+
+            var productId = await db.InsertProduct(request);
+            return new()
+            {
+                productId = productId,
+            };
+        }
+    }
+}
