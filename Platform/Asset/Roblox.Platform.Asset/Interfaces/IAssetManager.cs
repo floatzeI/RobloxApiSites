c87539096cf68c4398ef0acad0a34e464bcdadd6
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-18 18:54:54 -0400

    add docs for CreateAsset()

--- Platform/Asset/Roblox.Platform.Asset/Interfaces/IAssetManager.cs
+++ Platform/Asset/Roblox.Platform.Asset/Interfaces/IAssetManager.cs
@@ -4,6 +4,11 @@ namespace Roblox.Platform.Asset
 {
     public interface IAssetManager
     {
+        /// <summary>
+        /// Create an asset.
+        /// </summary>
+        /// <param name="request">The asset to create</param>
+        /// <returns>The <see cref="Models.CreateAssetResponse"/> corresponding to the asset</returns>
         Task<Models.CreateAssetResponse> CreateAsset(Models.CreateAssetRequest request);
     }
 }
