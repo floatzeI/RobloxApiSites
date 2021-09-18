17d6004d6be0c083f5e7ac2d08680c39802b7fd7
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-18 18:54:04 -0400

    add CreateAssetRequest; add CreateAssetResponse

---
+++ ApiClients/Roblox.Assets.Client/Models/CreateAsset.cs
@@ -0,0 +1,19 @@
+﻿using Roblox.Web.Enums;
+
+namespace Roblox.Assets.Client.Models
+{
+    public class CreateAssetRequest
+    {
+        public string name { get; set; }
+        public string description { get; set; }
+        public CreatorType creatorType { get; set; }
+        public long creatorId { get; set; }
+        public AssetType assetType { get; set; }
+    }
+
+    public class CreateAssetResponse
+    {
+        public long assetId { get; set; }
+    }
+
+}
