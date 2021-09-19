debf70d66c773119bf33893bc6e21bf9b0f88ba7
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-19 07:53:49 -0400

    add CreateRequest

---
+++ Services/Roblox.Services/Models/Ownership/Create.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Roblox.Services.Models.Ownership
+{
+    public class CreateRequest
+    {
+        public long assetId { get; set; }
+        public long userId { get; set; }
+        public int? serialNumber { get; set; }
+        public DateTime? expires { get; set; }
+    }
+}
