16134bf5f6aec755f7a90ca688ee13f7ca916eac
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-19 07:53:35 -0400

    implement IOwnershipService

---
+++ Services/Roblox.Services/Services/OwnershipService.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Roblox.Services.Database;
+using Roblox.Services.Models.Ownership;
+
+namespace Roblox.Services.Services
+{
+    public class OwnershipService : IOwnershipService
+    {
+        private IOwnershipDatabase db { get; set; }
+
+        public OwnershipService(IOwnershipDatabase db)
+        {
+            this.db = db;
+        }
+        
+        public async Task<OwnershipEntry> InsertEntry(CreateRequest request)
+        {
+            return await db.InsertEntry(request);
+        }
+
+        public async Task<bool> DoesUserOwnsAsset(long userId, long assetId)
+        {
+            var entries = await db.GetEntriesByUser(userId, assetId);
+            return entries.Any();
+        }
+    }
+}
