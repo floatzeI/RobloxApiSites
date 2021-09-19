abf15d4232415c01851b1ee9ddb94f98ae0a2d6a
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-19 07:53:13 -0400

    add OwnershipController

---
+++ Services/Roblox.Services/Controllers/V1/OwnershipController.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Roblox.Services.Services;
+
+namespace Roblox.Services.Controllers.V1
+{
+    [ApiController]
+    [Route("/Ownership")]
+    public class OwnershipController
+    {
+        private IOwnershipService ownershipService { get; set; }
+
+        public OwnershipController(IOwnershipService service)
+        {
+            ownershipService = service;
+        }
+
+        [HttpPost("InsertEntry")]
+        public async Task<Models.Ownership.OwnershipEntry> InsertEntry(
+            [Required, FromBody] Models.Ownership.CreateRequest request)
+        {
+            return await ownershipService.InsertEntry(request);
+        }
+
+        [HttpGet("AgentOwnsAsset")]
+        public async Task<Models.Ownership.OwnershipResponse> AgentOwnsAsset([Required] long agentId,
+            [Required] long assetId)
+        {
+            var status = await ownershipService.DoesUserOwnsAsset(agentId, assetId);
+            return new()
+            {
+                isOwner = status,
+            };
+        }
+    }
+}
