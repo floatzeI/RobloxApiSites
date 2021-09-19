0a472690ea3d3c02ff36d6fb7e93396ee291f0e7
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-19 07:53:21 -0400

    add OwnershipController (health check)

---
+++ Services/Roblox.Services/Controllers/OwnershipController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Roblox.Services.Controllers
+{
+    [ApiController]
+    [Route("/Ownership")]
+    public class OwnershipController
+    {
+        [HttpGet]
+        public Models.Shared.HealthCheckResponse GetStatus()
+        {
+            return new()
+            {
+                name = "Roblox.Ownership.Service",
+                status = "OK"
+            };
+        }
+    }
+}
