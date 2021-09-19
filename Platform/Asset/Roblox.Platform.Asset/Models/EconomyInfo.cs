18c7ce3591dd3cfa596b6e5eb4790cd44da6c955
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-19 07:19:55 -0400

    add limited params to EconomyInfo

--- Platform/Asset/Roblox.Platform.Asset/Models/EconomyInfo.cs
+++ Platform/Asset/Roblox.Platform.Asset/Models/EconomyInfo.cs
@@ -1,9 +1,14 @@
-﻿namespace Roblox.Platform.Asset.Models
+﻿using System;
+
+namespace Roblox.Platform.Asset.Models
 {
     public class EconomyInfo
     {
         public int? priceInRobux { get; set; }
         public int? priceInTickets { get; set; }
         public bool isForSale { get; set; }
+        public bool isLimited { get; set; }
+        public bool isLimitedUnique { get; set; }
+        public DateTime? offSaleDeadline { get; set; }
     }
 }
