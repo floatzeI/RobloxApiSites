b817c11f09291404bb9ca601b0ea142d9bd4778a
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-19 07:19:15 -0400

    add SetProduct

--- ApiClients/Roblox.Marketplace.Client/Interfaces/IMarketplaceV1Client.cs
+++ ApiClients/Roblox.Marketplace.Client/Interfaces/IMarketplaceV1Client.cs
@@ -9,7 +9,13 @@ namespace Roblox.Marketplace.Client
         /// Get products by their assetId.
         /// </summary>
         /// <param name="assetIds">An <see cref="IEnumerable{T}"/> of assetIds</param>
-        /// <returns>An <see cref="IEnumerable{T}"/> of <see cref="Models.ProductEntry"/></returns>
-        Task<IEnumerable<Models.ProductEntry>> GetProductsByAssetId(IEnumerable<long> assetIds);
+        /// <returns>An <see cref="IEnumerable{T}"/> of <see cref="Models.AssetEntry"/></returns>
+        Task<IEnumerable<Models.AssetEntry>> GetProductsByAssetId(IEnumerable<long> assetIds);
+        /// <summary>
+        /// Create or update the product for the assetId. The productId value is ignored.
+        /// </summary>
+        /// <param name="request">The request</param>
+        /// <returns>The ID of the created (or updated) product</returns>
+        Task<Models.CreateResponse> SetProduct(Models.AssetEntry request);
     }
 }
