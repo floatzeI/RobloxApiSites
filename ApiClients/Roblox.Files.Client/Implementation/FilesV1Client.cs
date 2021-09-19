21f2dc9dfae8390c55cf67915a0788c0d8b45de1
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-19 15:13:58 -0400

    fix various issues with initial imp

--- ApiClients/Roblox.Files.Client/Implementation/FilesV1Client.cs
+++ ApiClients/Roblox.Files.Client/Implementation/FilesV1Client.cs
@@ -1,6 +1,7 @@
 // I'm way too lazy to make ClientBase work with Multipart and streams. If anyone wants to refactor it to use ClientBase, go ahead...
 
 using System;
+using System.Globalization;
 using System.IO;
 using System.Net.Http;
 using System.Text.Json;
@@ -21,19 +22,20 @@ namespace Roblox.Files.Client
 
         public async Task<Stream> GetFileById(string fileId)
         {
-            return await clientBase.GetStreamAsync(baseUrl + "/v1/GetFile?fileId=" + fileId);
+            return await clientBase.GetStreamAsync(baseUrl + "v1/GetFile?fileId=" + fileId);
         }
 
         public async Task<string> UploadFile(string mimeType, Stream fileToUpload)
         {
             var content = new MultipartFormDataContent();
-            content.Add(new StringContent(mimeType), "mime");
-            content.Add(new StreamContent(fileToUpload), "file");
-            var response = await clientBase.PostAsync(baseUrl + "/v1/UploadFile", content);
-            if (!response.IsSuccessStatusCode)
-                throw new Exception("Unexpected Upload Response: " + response.StatusCode);
+            content.Add(new StringContent(mimeType), "mime", "mime");
+            content.Add(new StreamContent(fileToUpload), "file", "file");
+            var url = baseUrl + "v1/UploadFile";
+            var response = await clientBase.PostAsync(url, content);
             var txt = await response.Content.ReadAsStringAsync();
-            return JsonSerializer.Deserialize<Models.UploadResponse>(txt).fileId;
+            if (!response.IsSuccessStatusCode)
+                throw new Exception("Unexpected Upload Response: " + response.StatusCode + "\nURL = " + url + "\nBody=" + txt);
+            return JsonSerializer.Deserialize<Models.UploadResponse>(txt).id;
         }
     }
 }
