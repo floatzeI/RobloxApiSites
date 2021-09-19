dd41293f2413a9f52ce9b4f9d087b6d8ff7425db
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-19 07:53:53 -0400

    add IOwnershipService

---
+++ Services/Roblox.Services/Interfaces/Services/IOwnershipService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Roblox.Services.Services
+{
+    public interface IOwnershipService
+    {
+        Task<Models.Ownership.OwnershipEntry> InsertEntry(Models.Ownership.CreateRequest request);
+        Task<bool> DoesUserOwnsAsset(long userId, long assetId);
+    }
+}
