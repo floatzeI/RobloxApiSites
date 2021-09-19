cb0a200a5332dc32eea7af9608c985d3e6593d94
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-19 07:54:01 -0400

    add IOwnershipDatabase

---
+++ Services/Roblox.Services/Interfaces/Database/IOwnershipDatabase.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Roblox.Services.Database
+{
+    public interface IOwnershipDatabase
+    {
+        Task<Models.Ownership.OwnershipEntry> InsertEntry(Models.Ownership.CreateRequest request);
+        Task<IEnumerable<Models.Ownership.OwnershipEntry>> GetEntriesByUser(long userId, long assetId);
+    }
+}
