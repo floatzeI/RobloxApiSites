b85bb260d6553515dd10514f741b93f6c47abf57
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-20 12:46:04 -0400

    add Creator

---
+++ ApiSites/Roblox.ApiProxy.Api/Roblox.ApiProxy.Api/Models/Creator.cs
@@ -0,0 +1,12 @@
+﻿using Roblox.Web.Enums;
+
+namespace Roblox.ApiProxy.Api.Models
+{
+    public class Creator
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public CreatorType CreatorType { get; set; }
+        public long CreatorTargetId { get; set; }
+    }
+}
