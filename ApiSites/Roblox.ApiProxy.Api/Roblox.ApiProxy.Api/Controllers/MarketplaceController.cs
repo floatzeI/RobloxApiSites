e509c14fc10e5a23390f8a860c66742c12ca5522
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-20 12:52:15 -0400

    add various api clients;

--- ApiSites/Roblox.ApiProxy.Api/Roblox.ApiProxy.Api/Controllers/MarketplaceController.cs
+++ ApiSites/Roblox.ApiProxy.Api/Roblox.ApiProxy.Api/Controllers/MarketplaceController.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Roblox.Assets.Client;
+using Roblox.Marketplace.Client;
 
 namespace Roblox.ApiProxy.Api.Controllers
 {
@@ -8,10 +10,27 @@ namespace Roblox.ApiProxy.Api.Controllers
     [Route("/Marketplace")]
     public class MarketplaceController
     {
+        private IAssetsV1Client assetsClient { get; set; }
+        private IMarketplaceV1Client marketplaceClient { get; set; }
+
+        public MarketplaceController(IAssetsV1Client assetsV1Client, IMarketplaceV1Client marketplaceV1Client)
+        {
+            assetsClient = assetsV1Client;
+            marketplaceClient = marketplaceV1Client;
+        }
+        
         [HttpGet("GetProductInfo")]
         public async Task<Models.ProductInfoResponse> GetProductInfo([FromQuery] long assetId)
         {
-            throw new NotImplementedException();
+            var details = await assetsClient.GetAssetById(assetId);
+            var response = new Models.ProductInfoResponse()
+            {
+                AssetId = details.assetId,
+                Name = details.name,
+                Description = null, // todo
+                AssetTypeId = details.assetType,
+            };
+            return response;
         }
     }
 }
