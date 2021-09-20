7f88c2f307295948e9d2cd9f028f41cedaef4e78
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-20 13:13:55 -0400

    add validation (mostly for debugging) when setting assetTypeId

--- Services/Roblox.Services/Models/Assets/AssetEntry.cs
+++ Services/Roblox.Services/Models/Assets/AssetEntry.cs
@@ -5,7 +5,22 @@ namespace Roblox.Services.Models.Assets
     public class AssetEntry
     {
         public long assetId { get; set; }
-        public int assetTypeId { get; set; }
+
+        private int _assetTypeId { get; set; }
+        public int assetTypeId
+        {
+            get => _assetTypeId;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Invalid AssetTypeId: 0");
+                }
+
+                _assetTypeId = value;
+            }
+        }
+
         public string name { get; set; }
         public string description { get; set; }
         public int creatorType { get; set; }
