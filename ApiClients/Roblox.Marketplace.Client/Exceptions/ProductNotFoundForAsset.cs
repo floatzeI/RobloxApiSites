332c8706675ae51ee666fb56e6237f0bb43cadc9
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-20 13:04:58 -0400

    add ProductNotFoundForAsset

---
+++ ApiClients/Roblox.Marketplace.Client/Exceptions/ProductNotFoundForAsset.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Roblox.Marketplace.Client.Model
+{
+    public class ProductNotFoundForAsset : Exception
+    {
+        public ProductNotFoundForAsset(long assetId, Exception inner) : base("A product could not be found for this assetId.\nID = " + assetId, inner)
+        {
+            
+        }
+    }
+}
