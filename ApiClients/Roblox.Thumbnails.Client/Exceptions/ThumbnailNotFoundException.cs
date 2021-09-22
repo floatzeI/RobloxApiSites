a1fc298a4c32069bb7316171857ead12689507ef
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-22 19:01:25 -0400

    add ThumbnailNotFoundException

---
+++ ApiClients/Roblox.Thumbnails.Client/Exceptions/ThumbnailNotFoundException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Roblox.Thumbnails.Client
+{
+    public class ThumbnailNotFoundException : Exception
+    {
+        public ThumbnailNotFoundException(long referenceId, Exception inner) : base(
+            "Thumbnail not found with Reference ID = " + referenceId, inner)
+        {
+            
+        }
+        
+        public ThumbnailNotFoundException(string thumbnailHash, Exception inner) : base(
+            "Thumbnail not found with Hash = " + thumbnailHash, inner)
+        {
+            
+        }
+    }
+}
