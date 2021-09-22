e865cb164382bf419e0b9c618670873ac22d229c
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-22 19:01:38 -0400

    add IThumbnailsV1Client

---
+++ ApiClients/Roblox.Thumbnails.Client/Interfaces/IThumbnailsV1Client.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Roblox.Thumbnails.Client.Models;
+
+namespace Roblox.Thumbnails.Client
+{
+    public interface IThumbnailsV1Client
+    {
+        Task<ThumbnailEntry> GetThumbnail(long referenceId, ThumbnailType type, int resolutionX, int resolutionY);
+        Task<ThumbnailEntry> GetThumbnailByHash(string hash, int resolutionX, int resolutionY);
+        Task DeleteThumbnail(long referenceId, ThumbnailType type);
+        Task InsertThumbnail(ThumbnailEntry thumbnail);
+    }
+}
