b959608045068feeb9ca2fbdbfbd29c91d3d8766
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-22 13:02:40 -0400

    add IRenderingClient

---
+++ ApiClients/Roblox.Rendering.Client/Interfaces/IRenderingClient.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Roblox.Rendering.Client.Models;
+
+namespace Roblox.Rendering.Client
+{
+    public interface IRenderingClient
+    {
+        /// <summary>
+        /// Render an avatar thumbnail
+        /// </summary>
+        /// <remarks>
+        /// This operation is slow. It may take anywhere from 5 seconds to 60 seconds, or more, to return a response.
+        /// </remarks>
+        /// <param name="request">The <see cref="Models.RenderAvatarRequest"/></param>
+        /// <returns>The <see cref="Models.RenderAvatarResponse"/></returns>
+        Task<RenderAvatarResponse> RenderAvatarThumbnail(RenderAvatarRequest request);
+    }
+}
