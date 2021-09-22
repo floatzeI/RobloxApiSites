6802f11d14fa39fe171972e0b4490fc4813ab7f4
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-22 13:02:50 -0400

    implement IRenderingClient;

---
+++ ApiClients/Roblox.Rendering.Client/Implementation/RenderingClient.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using Roblox.Rendering.Client.Models;
+using StackExchange.Redis;
+
+namespace Roblox.Rendering.Client
+{
+    public class RenderingClient : IRenderingClient
+    {
+        private static ConnectionMultiplexer redisConnectionMultiplexer { get; set; }
+
+        private static IDatabase redisDb => redisConnectionMultiplexer.GetDatabase(0);
+
+        private const string renderChannelRequest = "RenderServiceRequest_V1";
+        private const string renderChannelResponse = "RenderServiceResponse_V1";
+        
+        // pooling
+
+        private static Mutex renderPoolMutex { get; set; } = new();
+
+        private static List<RenderPoolEntry> renderPool { get; set; } = new();
+
+        public RenderingClient(ConnectionMultiplexer redis)
+        {
+            redisConnectionMultiplexer = redis;
+            RenderingClient.RedisSubscribeHandler();
+        }
+
+        private static async Task RedisSubscribeHandler()
+        {
+            var con = redisConnectionMultiplexer.GetSubscriber();
+            var channel = await con.SubscribeAsync(renderChannelResponse);
+            channel.OnMessage(message =>
+            {
+                if (!message.Message.HasValue) return;
+                var decoded = JsonSerializer.Deserialize<RenderResponse>(message.Message);
+                if (decoded == null) return;
+                // broadcast
+                renderPoolMutex.WaitOne();
+                foreach (var item in renderPool)
+                {
+                    if (item.id == decoded.id)
+                    {
+                        try
+                        {
+                            item.onFinish(decoded, message.Message);
+                        }
+                        catch (Exception)
+                        {
+                            // todo: log this somehow
+                        }
+                    }
+                }
+                renderPool = renderPool.Where(c => c.id != decoded.id).ToList();
+                renderPoolMutex.ReleaseMutex();
+            });
+        }
+
+        private async Task SendRequest(RenderRequest request)
+        {
+            await redisDb.PublishAsync(renderChannelRequest, JsonSerializer.Serialize(request));
+        }
+        
+        public Task<RenderAvatarResponse> RenderAvatarThumbnail(RenderAvatarRequest request)
+        { 
+            var id = Guid.NewGuid().ToString();
+            var tcs = new TaskCompletionSource<RenderAvatarResponse>();
+            
+            renderPoolMutex.WaitOne();
+            renderPool.Add(new ()
+            {
+                id = id,
+                parameters = new()
+                {
+                    request,
+                },
+                onFinish = (onFinish, rawBody) =>
+                {
+                    if (onFinish.status == 200)
+                    {
+                        tcs.SetResult(new ()
+                        {
+                            fileBase64 = onFinish.data,
+                        });
+                    }
+                    else
+                    {
+                        tcs.SetException(new Exception("Result failed with Status = " + onFinish.status + "\nBody = " + rawBody + "\nID = " + onFinish.id));
+                    }
+                    return null;
+                },
+            });
+            renderPoolMutex.ReleaseMutex();
+
+            Task.Run(async () =>
+            {
+                await SendRequest(new RenderRequest()
+                {
+                    id = id,
+                    args = new()
+                    {
+                        request,
+                    },
+                    command = "GenerateThumbnail",
+                });
+            });
+            
+            return tcs.Task;
+        }
+    }
+}
