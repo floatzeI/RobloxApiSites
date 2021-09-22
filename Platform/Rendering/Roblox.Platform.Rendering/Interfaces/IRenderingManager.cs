128a65fe20b8cad772b40994b1836ca3ac851a91
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-22 19:04:43 -0400

    create IRenderingManager

---
+++ Platform/Rendering/Roblox.Platform.Rendering/Interfaces/IRenderingManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Roblox.Avatar.Client.Models;
+
+namespace Roblox.Platform.Rendering
+{
+    public interface IRenderingManager
+    {
+        /// <summary>
+        /// Delete all thumbnails and icons for the userId
+        /// </summary>
+        /// <param name="userId">The userId</param>
+        Task DeleteAvatarThumbnails(long userId);
+        /// <summary>
+        /// Render an avatar thumbnail and save it to the database.
+        /// </summary>
+        /// <param name="userId">The id of the user</param>
+        /// <param name="resolution">The resolution</param>
+        /// <param name="avatar">The avatar of the user</param>
+        /// <param name="force">If true, a new render will be forced</param>
+        Task RenderAvatarThumbnail(long userId, int resolution, AvatarEntry avatar, bool force = false);
+
+        /// <summary>
+        /// Render an avatar headshot and save it to the database/
+        /// </summary>
+        /// <param name="userId">The ID of the user</param>
+        /// <param name="resolution">The resolution</param>
+        /// <param name="avatar">The avatar of the user</param>
+        /// <param name="force">If true, a new render will be forced</param>
+        Task RenderAvatarHeadshot(long userId, int resolution, AvatarEntry avatar, bool force = false);
+    }
+}
