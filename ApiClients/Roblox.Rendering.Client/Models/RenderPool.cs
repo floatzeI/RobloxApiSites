730cee9c59045251f02600585f0f389984933fc9
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-22 13:02:22 -0400

    add RenderPoolEntry

---
+++ ApiClients/Roblox.Rendering.Client/Models/RenderPool.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Roblox.Rendering.Client.Models
+{
+    public class RenderPoolEntry
+    {
+        public string command { get; set; }
+        public string id { get; set; }
+        public List<dynamic> parameters { get; set; }
+        public Func<RenderResponse, string, object> onFinish { get; set; }
+    }
+}
