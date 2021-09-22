6d888bb9a27b9aecbe5c7e5cfbaf93b701fa35c8
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-22 19:06:39 -0400

    change gender param (enum => byte)

--- Services/Roblox.Services/Interfaces/Services/IUsersService.cs
+++ Services/Roblox.Services/Interfaces/Services/IUsersService.cs
@@ -9,7 +9,7 @@ namespace Roblox.Services.Services
         Task<Models.Users.UserDescriptionEntry> GetDescription(long userId);
         Task SetUserDescription(long userId, string description);
         Task SetUserBirthDate(long userId, DateTime birthDate);
-        Task SetUserGender(long userId, Gender gender);
+        Task SetUserGender(long userId, byte gender);
         Task<Models.Users.UserInformationResponse> GetUserById(long userId);
         DateTime GetDateTimeFromBirthDate(int birthYear, int birthMonth, int birthDay);
         Task<Models.Users.UserAccountEntry> CreateUser(string username);
