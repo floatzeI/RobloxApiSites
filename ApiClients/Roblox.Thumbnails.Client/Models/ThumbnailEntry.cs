466be8b8ac6f39a9a3add4f5081a6816b796a7b5
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-22 19:01:43 -0400

    add ThumbnailEntry

---
+++ ApiClients/Roblox.Thumbnails.Client/Models/ThumbnailEntry.cs
@@ -0,0 +1,22 @@
+﻿namespace Roblox.Thumbnails.Client.Models
+{
+    public class ThumbnailEntry
+    {
+        public ThumbnailEntry(string thumbnailId, long referenceId, string fileId, ThumbnailType thumbnailType,
+            int resolutionX, int resolutionY)
+        {
+            this.thumbnailId = thumbnailId;
+            this.referenceId = referenceId;
+            this.fileId = fileId;
+            this.thumbnailType = thumbnailType;
+            this.resolutionX = resolutionX;
+            this.resolutionY = resolutionY;
+        }
+        public string thumbnailId { get; set; }
+        public long referenceId { get; set; }
+        public string fileId { get; set; }
+        public ThumbnailType thumbnailType { get; set; }
+        public int resolutionX { get; set; }
+        public int resolutionY { get; set; }
+    }
+}
