6bb30017fc67274d430feb762e2451235713b305
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-22 13:02:31 -0400

    add RenderAvatarResponse; add RenderAvatarRequest;

---
+++ ApiClients/Roblox.Rendering.Client/Models/RenderAvatar.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Roblox.Rendering.Client.Models
+{
+    public class RenderAvatarResponse
+    {
+        public string fileBase64 { get; set; }
+    }
+
+    public class RenderAssetEntry
+    {
+        public long id { get; set; }
+    }
+
+    public class RenderBodyColors
+    {
+        public int headColorId { get; set; }
+        public int torsoColorId { get; set; }
+        public int rightArmColorId { get; set; }
+        public int leftArmColorId { get; set; }
+        public int rightLegColorId { get; set; }
+        public int leftLegColorId { get; set; }
+    }
+
+    public class RenderAvatarRequest
+    {
+        public long userId { get; set; }
+        public RenderBodyColors bodyColors { get; set; }
+        public string playerAvatarType { get; set; }
+        public IEnumerable<RenderAssetEntry> assets { get; set; }
+    }
+}
