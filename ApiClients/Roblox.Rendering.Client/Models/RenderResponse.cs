10b17b94b23d91a0cb3162424639cd86979994f9
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-22 13:02:13 -0400

    add RenderResponse

---
+++ ApiClients/Roblox.Rendering.Client/Models/RenderResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Roblox.Rendering.Client.Models
+{
+    public class RenderResponse
+    {
+        public int status { get; set; }
+        public string id { get; set; }
+        public string data { get; set; }
+    }
+}
