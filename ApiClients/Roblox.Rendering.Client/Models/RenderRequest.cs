49e6ab3345ce09b43272fa0ce3649c3833d87b1b
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-22 13:02:18 -0400

    add RenderRequest

---
+++ ApiClients/Roblox.Rendering.Client/Models/RenderRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Roblox.Rendering.Client.Models
+{
+    public class RenderRequest
+    {
+        public string command { get; set; }
+        public List<dynamic> args { get; set; }
+        public string id { get; set; }
+    }
+}
