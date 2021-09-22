545cec6808ed961418fdb64c74febe4c208ae90e
floatzeI <27046219+floatzeI@users.noreply.github.com>
2021-09-22 19:05:04 -0400

    implement UploadPngThumbnail(); add thumbnailsClient;

--- Platform/Thumbnail/Roblox.Platform.Thumbnail/Implmentation/ThumbnailManager.cs
+++ Platform/Thumbnail/Roblox.Platform.Thumbnail/Implmentation/ThumbnailManager.cs
@@ -1,25 +1,27 @@
 using System.Threading.Tasks;
 using Roblox.Files.Client;
 using Roblox.Platform.Thumbnail.Models;
+using Roblox.Thumbnails.Client;
 
 namespace Roblox.Platform.Thumbnail
 {
     public class ThumbnailManager : IThumbnailManager
     {
         private IFilesV1Client filesClient { get; set; }
+        private IThumbnailsV1Client thumbnailsClient { get; set; }
 
-        public ThumbnailManager(IFilesV1Client filesV1Client)
+        public ThumbnailManager(IFilesV1Client filesV1Client, IThumbnailsV1Client thumbnailsV1Client)
         {
             filesClient = filesV1Client;
+            thumbnailsClient = thumbnailsV1Client;
         }
 
 
         public async Task UploadPngThumbnail(ThumbnailUploadRequest request)
         {
             request.mime = "image/png";
-            // todo: first, upload thumbnail to files service
-            // then send the data over to thumbnail service
-            throw new System.NotImplementedException();
+            var fileId = await filesClient.UploadFile(request.mime, request.file);
+            await thumbnailsClient.InsertThumbnail(new(request.id,request.referenceId, fileId, request.thumbnailType, request.resolutionX, request.resolutionY));
         }
     }
 }
